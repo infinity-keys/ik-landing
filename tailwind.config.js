15f32df3e05cb4e4f7dde1366144c50840e3c9cb
Christopher Bloom <bloomcb@gmail.com>
2022-01-19 12:52:03 -0800

    Addition of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*{pages,components}/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
