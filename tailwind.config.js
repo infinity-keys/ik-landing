a4f11a46d41037f5e6a23c5a84ac827ac6c90d86
Christopher Bloom <bloomcb@gmail.com>
2022-01-19 13:24:08 -0800

    eslint config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    "./*{pages,components}/**/*.{js,ts,jsx,tsx}",
-    // "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./*{pages,components}/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
