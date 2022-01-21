e8bb0b7a9c07425195498909188c70c694367568
Christopher Bloom <bloomcb@gmail.com>
2022-01-21 06:40:13 -0800

    Optimize image

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,28 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="ik-page flex flex-col items-center justify-center min-h-screen bg-cover bg-center">
+    <div className="ik-page flex flex-col items-center justify-center min-h-screen relative">
       <Head>
         <title>Infinity Keys</title>
         <meta name="description" content="Keys keys keys" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <Image
+        src="/bg.jpeg"
+        alt="random background image"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center z-10">
         <h1 className="text-6xl font-bold text-white drop-shadow-lg">
           Infinity Keys
         </h1>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24 border-t z-10">
         <p>
           Photo by{" "}
           <a href="https://unsplash.com/@socialcut?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
