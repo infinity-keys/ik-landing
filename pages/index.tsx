785cbbd6f9b7f3c6f5505eb5e8e8462a58996691
Christopher Bloom <bloomcb@gmail.com>
2022-01-21 23:45:24 -0800

    Initial auth0 and text input code example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useUser } from "@auth0/nextjs-auth0";
+// @TODO: bring in d.ts for this since it does not have types published
+import RICIBs from "react-individual-character-input-boxes";
+
+/* Disable ESLint for this file because auth needs full page loading */
+/*eslint-disable @next/next/no-html-link-for-pages*/
 
 const Home: NextPage = () => {
+  const { user, error, isLoading } = useUser();
+
+  const handleOutput = (output: string) => {
+    console.log(output);
+  };
+
   return (
     <div className="ik-page flex flex-col items-center justify-center min-h-screen relative">
       <Head>
@@ -23,6 +35,34 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold text-white drop-shadow-lg">
           Infinity Keys
         </h1>
+
+        <div className="magic-input pt-9">
+          <RICIBs
+            amount={5}
+            handleOutputString={handleOutput}
+            inputRegExp={/^[0-9]$/}
+          />
+        </div>
+
+        <p className="pt-6">- or -</p>
+
+        <div className="auth pt-8">
+          {isLoading && <p>Loading...</p>}
+          {error && <p>Error: {error.message}</p>}
+          {!user && !isLoading && (
+            <a className="font-bold" href="/api/auth/login">
+              Login.
+            </a>
+          )}
+          {user && (
+            <p>
+              Welcome {user?.given_name}.{" "}
+              <a className="font-bold" href="/api/auth/logout">
+                Logout.
+              </a>
+            </p>
+          )}
+        </div>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t z-10">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+const tailwindforms = require("@tailwindcss/forms");
+
 module.exports = {
   content: ["./*{pages,components}/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindforms],
 };
