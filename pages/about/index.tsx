b2abbe3b7a7cceeb55dac3b791e47246c1a34849
Christopher Bloom <bloomcb@gmail.com>
2022-03-17 22:19:06 -0700

    Inner pages

---
+++ pages/about/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+  return <div className="container mx-auto">About</div>;
+};
+
+export default About;
---
+++ pages/about/mission.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Mission: NextPage = () => {
+  return <div className="container mx-auto">Mission</div>;
+};
+
+export default Mission;
