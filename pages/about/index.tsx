11fc258061936caadbe997d40b3a8d2483d06bb0
Christopher Bloom <bloomcb@gmail.com>
2022-03-18 11:38:28 -0700

    Proper svg props

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -19,6 +19,8 @@ import {
 } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 
+type IconProps = {};
+
 const solutions = [
   {
     name: "Inbox",
@@ -151,7 +153,7 @@ const footerNavigation = {
     {
       name: "Facebook",
       href: "#",
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -164,7 +166,7 @@ const footerNavigation = {
     {
       name: "Instagram",
       href: "#",
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -177,7 +179,7 @@ const footerNavigation = {
     {
       name: "Twitter",
       href: "#",
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
         </svg>
@@ -186,7 +188,7 @@ const footerNavigation = {
     {
       name: "GitHub",
       href: "#",
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -199,7 +201,7 @@ const footerNavigation = {
     {
       name: "Dribbble",
       href: "#",
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
