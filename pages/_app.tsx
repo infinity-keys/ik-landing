3a49ce6fb829f433564c37c181c390966341a24a
Christopher Bloom <bloomcb@gmail.com>
2022-04-18 20:40:09 -0700

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 // Re-enable here and below for auth via auth0
 // import { UserProvider } from "@auth0/nextjs-auth0";
@@ -10,9 +11,24 @@ import "../styles/scanlines.scss";
 
 function InfinityKeysApp({ Component, pageProps }: AppProps) {
   return (
-    // <UserProvider>
-    <Component {...pageProps} />
-    // </UserProvider>
+    <>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.ga=window.ga||function(){(ga.q = ga.q || []).push(arguments)};
+          ga.l=+new Date;
+          ga('create', '${process.env.INFINITY_KEYS_ACCESS_CODE}', 'auto');
+          ga('send','pageview');
+        `}
+      </Script>
+
+      <Script
+        async
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="afterInteractive"
+      ></Script>
+
+      <Component {...pageProps} />
+    </>
   );
 }
 
