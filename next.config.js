d547ae5f9a0960bd18db8bc98104b6a67da115e3
Christopher Bloom <bloomcb@gmail.com>
2022-05-11 22:56:52 -0700

    proper rewrites

--- next.config.js
+++ next.config.js
@@ -4,12 +4,14 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "tailwindui.com"],
   },
-  exportPathMap: () => ({
-    // The root page is just a puzzle
-    "/": { page: "/puzzle/landing" },
-    // Forward old avalanche page to new
-    "/avalanche": { page: "/puzzle/avalanche" },
-  }),
+  async rewrites() {
+    return [
+      // The root page is just a puzzle
+      { source: "/", destination: "/puzzle/landing" },
+      // Forward old avalanche page to new
+      { source: "/avalanche", destination: "/puzzle/avalanche" },
+    ];
+  },
 };
 
 module.exports = nextConfig;
