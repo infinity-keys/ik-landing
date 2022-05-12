c0ad402efea17c354d45c1ace7f8483cb4242fd6
Christopher Bloom <bloomcb@gmail.com>
2022-05-12 10:50:38 -0700

    Wrong guess message

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -7,6 +7,7 @@ import MaterialIcon from "@components/svg/material-lock-svg";
 
 import { PuzzleApis } from "@lib/types";
 import { puzzlePost } from "@lib/fetchers";
+import clsx from "clsx";
 
 interface PuzzleProps {
   count: number;
@@ -21,10 +22,12 @@ const Puzzle = ({ count, puzzleUri, boxes = true }: PuzzleProps) => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [charsLeft, setCharsLeft] = useState(count);
+  const [isWrongGuess, setIsWrongGuess] = useState(false);
   const router = useRouter();
 
   const handleInput = async (input: string) => {
     setCharsLeft(count - input.length);
+    setIsWrongGuess(false);
     // Bail if guess is not the length of input
     if (input.length !== count) return;
 
@@ -39,6 +42,7 @@ const Puzzle = ({ count, puzzleUri, boxes = true }: PuzzleProps) => {
     const { fail_route, success_route } = await res.json();
     // Turn off loading if failed
     !success_route && setIsLoading(false);
+    !success_route && setIsWrongGuess(true);
     // debugger;
     router.push(success_route || fail_route);
   };
@@ -56,13 +60,16 @@ const Puzzle = ({ count, puzzleUri, boxes = true }: PuzzleProps) => {
       {!isLoading && (
         <div className="flex justify-center z-10">
           <div>
-            <div className="flex">
+            <div className="flex pb-5">
               <div className="w-6">
                 <MaterialIcon />
               </div>
               <h1 className="text-base font-bold pt-2 pl-4">Passcode</h1>
             </div>
-            <div className="magic-input pt-7 text-turquoise font-bold text-5xl">
+            <div className={clsx({ invisible: !isWrongGuess })}>
+              <span className="opacity-50">Incorrect passcode. Try again.</span>
+            </div>
+            <div className="magic-input pt-2 text-turquoise font-bold text-5xl">
               {boxes && (
                 <RICIBs
                   amount={count}
