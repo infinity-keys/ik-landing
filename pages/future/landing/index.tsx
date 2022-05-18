76cce20a7f9323edaaa5d4fb2c9b38582fb36076
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2022-05-18 14:29:18 -0400

    remove addtl button from header

--- pages/future/landing/index.tsx
+++ pages/future/landing/index.tsx
@@ -137,12 +137,6 @@ const Landing: NextPage<PageProps> = ({ count }) => {
               <div className="ml-10 space-x-4">
                 <a
                   href="#"
-                  className="inline-block bg-turquoise hover:bg-turquoiseDark py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
-                >
-                  Sign in
-                </a>
-                <a
-                  href="#"
                   className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-turquoise hover:bg-indigo-50"
                 >
                   Sign up
