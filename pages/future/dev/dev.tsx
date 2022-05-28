381dd233b279ff79f205ca66ed3de56fa49e285d
Christopher Bloom <bloomcb@gmail.com>
2022-05-28 00:05:02 -0700

    Tune up the wallet code, pop sign on connect

--- pages/future/dev/dev.tsx
+++ pages/future/dev/dev.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import Head from "next/head";
 import { useState, useEffect } from "react";
 
@@ -10,8 +9,6 @@ import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
 import Fortmatic from "fortmatic";
-import { Web3Provider } from "@ethersproject/providers";
-import { verify } from "crypto";
 
 const customNetworkOptions = {
   rpcUrl: 'https://rinkeby.infura.io/v3/c10d222a5bae4a8e97fad0915b06ff5d',
@@ -54,33 +51,39 @@ const web3Modal: Web3Modal | undefined =
     providerOptions
   }) : undefined
 
-// Scoped up here since multiple callbacks need it
-let library: ethers.providers.Web3Provider | undefined;
-
-
 const Dev: NextPage = () => {
-
   const [account, setAccount] = useState<string>();
   const [chainId, setChainId] = useState<number>();
 
-  //FOR SIGNING- NOT NEEDED FOR WALLET CONNECT
   const [signature, setSignature] = useState<string>('');
   const [verifiedAddress, setVerifiedAddress] = useState<string>('');
 
-  const connect = async (): Promise<Web3Provider | undefined> => {
-    if (!web3Modal) return;
-
+  const connect = async (): Promise<void> => {
     try {
+      if (!web3Modal) throw new Error('No web3Modal');
+
       const provider: ethers.providers.ExternalProvider = await web3Modal.connect();
-      library = new ethers.providers.Web3Provider(provider);
+      const library = new ethers.providers.Web3Provider(provider);
 
       const accounts = await library.listAccounts();
-      const network = await library.getNetwork();
-      accounts && setAccount(accounts[0]);
+      if (!accounts.length)
+        throw new Error("No accounts found");
+      const [account] = accounts
+      setAccount(account);
 
+      if (!library?.provider?.request)
+        throw new Error("Library is undefined");
+      const signature = await library?.provider?.request({
+        method: "personal_sign",
+        params: [message, account]
+      });
+      setSignature(signature);
+
+      const network = await library.getNetwork();
       setChainId(network.chainId);
     } catch (error) {
       console.log(error);
+      disconnect();
     }
 
   }
@@ -95,22 +98,6 @@ const Dev: NextPage = () => {
     setVerifiedAddress("");
   };
 
-  //FOR SIGNING ONLY- NOT NEEDED FOR WALLET CONNECTION
-  const signMessage = async () => {
-    if (!library?.provider?.request) return;
-
-    try {
-      const signature = await library.provider.request({
-        method: "personal_sign",
-        params: [message, account]
-      });
-      setSignature(signature);
-    } catch (error) {
-      console.log(error)
-    }
-
-  };
-
   const verifySignature = async () => {
     setVerifiedAddress(ethers.utils.verifyMessage(message, signature));
   }
@@ -123,36 +110,29 @@ const Dev: NextPage = () => {
 
       <div className="ik-page radial-bg ">
         <div className="container px-4 flex flex-col items-center justify-center min-h-screen max-w-sm ">
-          <header className="pt-4 md:pt-14 pb-4 block w-full">
-            <Image src="/logo.svg" width={100} height={63} alt="IK logo" />
-          </header>
 
-          <main className="flex flex-col items-center justify-center text-center w-full flex-1 z-10 ">
-            {account
-              ? <button onClick={disconnect}>Disconnect Wallet</button>
-              : <button onClick={connect}>Connect Wallet</button>}
+          <main className="flex flex-col items-center justify-center text-center w-full flex-1">
+            <button onClick={account ? disconnect : connect} className="text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4" >
+              {account ? 'Disconnect wallet' : 'Connect wallet'}
+            </button>
 
             {account && <div>
               <p>Wallet Address: {account}</p>
               <p>Chain Id: {chainId}</p>
-              <br />
-              {/*ONLY NEEDED FOR SIGNING MESSAGES, NOT WALLET CONNECTION*/}
-              {!signature
-                ? <button onClick={signMessage}>Sign Message</button>
-                : <div>
-                  <p>Signed Message: <br />{message}</p>
-                  <p>Signature: <br />{signature}</p>
-                </div>}
+
               {signature &&
                 <div>
-                  <br />
-                  <button onClick={verifySignature}>Verify Signature</button>
+                  <p>Signed Message: {message}</p>
+                  <p>Signature: {signature}</p>
+
+                  <button onClick={verifySignature} className="text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4">
+                    Verify Signature
+                  </button>
                   {verifiedAddress && <div>
-                    <p>Address of Signature: <br />{verifiedAddress}</p>
-                    <p>Address Match?<br />{verifiedAddress === account ? <span>True</span> : <span>False</span>}</p>
+                    <p>Address of Signature: {verifiedAddress}</p>
+                    <p>Address Match? <span>{verifiedAddress === account ? 'True' : 'False'}</span></p>
                   </div>}
                 </div>}
-              {/*END*/}
             </div>}
           </main>
         </div>
