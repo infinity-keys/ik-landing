5779292506145731e9a19e11be6bc6f8e2c9edc3
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2022-06-02 15:01:09 -0400

    adjust svg for mobile screens

--- components/svg/map-svg.tsx
+++ components/svg/map-svg.tsx
@@ -1,8 +1,8 @@
 const Map = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="100%"
-    height="100%"
+    width="relative"
+    height="10rem"
     viewBox="0 0 576 512"
   >
     <path
--- pages/future/landing/index.tsx
+++ pages/future/landing/index.tsx
@@ -320,12 +320,12 @@ const Landing: NextPage<PageProps> = ({ count, name }) => {
           /> */}
 
           <div className="justify-items-center items-center lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24 relative">
-            <div className="lg:w-90 lg:h-90 lg:col-start">
+            <div className="lg:w-90 lg:h-90 lg:col-start p-10">
               <Map />
             </div>
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
               <div>
-                <div className="mt-6">
+                <div className="mt-6 p-5">
                   <h2 className="text-3xl font-extrabold tracking-tight">
                     Build an Infinity Keys Hunt for your Project
                   </h2>
