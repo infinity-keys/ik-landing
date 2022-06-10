1041f6820bc72d61bbb245db02ca02f90583333e
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2022-06-10 13:22:56 -0400

    code cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 
 import "loaders.css/loaders.min.css";
 import "../styles/globals.css";
-import "../styles/scanlines.scss";
+// import "../styles/scanlines.scss";
 // import "../styles/stars.scss";
 
 function InfinityKeysApp({ Component, pageProps }: AppProps) {
