dff4a6bb359bc33227fa7c95e21b108fc2499145
Tawnee <allprt89@gmail.com>
2022-06-22 15:57:18 -0600

    cleanup conditionals and classes. add secondary button

--- pages/future/dev/claim-flow.tsx
+++ pages/future/dev/claim-flow.tsx
@@ -21,6 +21,12 @@ const ClaimFlow: NextPage = () => {
     return () => clearTimeout(timer);
   };
 
+  const buttonClasses =
+    "text-sm text-turquoise border-solid border-2 border-turquoise bg-transparent font-bold bg-turquoise hover:bg-turquoiseDark hover:text-blue rounded-md py-2 px-4 mt-6";
+
+  const buttonPrimaryClasses =
+    "text-sm text-blue font-bold bg-turquoise border-solid border-2 border-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4 mt-6";
+
   return (
     <Wrapper>
       <Head>
@@ -38,46 +44,33 @@ const ClaimFlow: NextPage = () => {
                 variant="marble"
                 colors={["#101D42", "#E400FF", "#3FCCBB", "#8500AC", "#303B5B"]}
               />
-              {!provider ? (
+              <h2 className="mt-4 text-xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-2xl lg:mt-8 xl:text-2xl">
+                {isLoading
+                  ? "Minting Trophy"
+                  : minted
+                  ? "Your Trophy Is Minted"
+                  : "Mint Your Trophy"}
+              </h2>
+
+              {!provider && (
+                <button
+                  className={buttonPrimaryClasses}
+                  type="submit"
+                  value="Join the mailing list"
+                  onClick={() => setProvider("ethereum")}
+                >
+                  Connect Wallet
+                </button>
+              )}
+
+              {!isLoading && provider && !minted && (
                 <>
-                  <h2 className="mt-4 text-xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-2xl lg:mt-8 xl:text-2xl">
-                    Mint Your Trophy
-                  </h2>
                   <button
-                    className="text-sm text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4 my-6"
-                    type="submit"
-                    value="Join the mailing list"
-                    onClick={() => setProvider("ethereum")}
-                  >
-                    connect wallet
-                  </button>
-                </>
-              ) : isLoading ? (
-                <div className="loader mx-auto mt-10">
-                  <div className="ball-clip-rotate-multiple">
-                    <div></div>
-                    <div></div>
-                  </div>
-                </div>
-              ) : minted ? (
-                <>
-                  <h2 className="mt-4 text-xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-2xl lg:mt-8 xl:text-2xl">
-                    Your Trophy Is Minted
-                  </h2>
-                  <a
-                    href="#"
-                    className="text-sm text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4 my-6"
-                  >
-                    View On {provider === "ethereum" ? "OpenSea" : "JoePegs"}
-                  </a>
-                </>
-              ) : (
-                <>
-                  <h2 className="mt-4 text-xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-2xl lg:mt-8 xl:text-2xl">
-                    Mint Your Trophy
-                  </h2>
-                  <button
-                    className="text-sm text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4 my-6"
+                    className={
+                      provider === "ethereum"
+                        ? buttonPrimaryClasses
+                        : buttonClasses
+                    }
                     type="submit"
                     value="Join the mailing list"
                     onClick={() => {
@@ -89,11 +82,15 @@ const ClaimFlow: NextPage = () => {
                     }}
                   >
                     {provider === "ethereum"
-                      ? "mint on ethereum"
-                      : "switch to ethereum"}
+                      ? "Mint on Ethereum"
+                      : "Switch to Ethereum"}
                   </button>
                   <button
-                    className="text-sm text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4"
+                    className={
+                      provider === "avalanche"
+                        ? buttonPrimaryClasses
+                        : buttonClasses
+                    }
                     type="submit"
                     value="Join the mailing list"
                     onClick={() => {
@@ -105,10 +102,25 @@ const ClaimFlow: NextPage = () => {
                     }}
                   >
                     {provider === "avalanche"
-                      ? "mint on avalanche"
-                      : "switch to avalanche"}
+                      ? "Mint on Avalanche"
+                      : "Switch to Avalanche"}
                   </button>
                 </>
+              )}
+
+              {isLoading && (
+                <div className="loader mx-auto mt-10">
+                  <div className="ball-clip-rotate-multiple">
+                    <div></div>
+                    <div></div>
+                  </div>
+                </div>
+              )}
+
+              {minted && (
+                <a href="#" className={buttonPrimaryClasses}>
+                  View On {provider === "ethereum" ? "OpenSea" : "JoePegs"}
+                </a>
               )}
             </div>
           </div>
