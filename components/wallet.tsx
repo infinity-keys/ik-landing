4129ef20141895b2496b264256d05ced0bc7eae2
Tawnee <allprt89@gmail.com>
2022-06-27 20:01:41 -0600

    move sign into utils

--- components/wallet.tsx
+++ components/wallet.tsx
@@ -1,137 +1,29 @@
-// import { useState, useEffect } from "react";
-
-// import { message } from "@lib/utils";
-// import { walletUtil } from "@lib/wallet";
-
-// const wallet = walletUtil();
-
-// interface WalletProps {
-//   onWalletSignature?: (address: string) => Promise<void>;
-// }
-// const Wallet = ({ onWalletSignature }: WalletProps) => {
-//   const [account, setAccount] = useState<string>();
-
-//   const [signature, setSignature] = useState<string>("");
-
-//   const connect = async (): Promise<void> => {
-//     try {
-//       const { library, account } = await wallet.trigger();
-
-//       setAccount(account);
-
-//       // Fetch our nonce used to sign our message
-//       const nonceReq = await fetch(`/api/users/${account}`);
-//       const { nonce } = await nonceReq.json();
-
-//       if (!library?.provider?.request) throw new Error("Library is undefined");
-//       const signature = await library.provider.request({
-//         method: "personal_sign",
-//         params: [message(nonce), account],
-//       });
-//       setSignature(signature);
-
-//       // With public address + signature, we can verify the signature
-//       const authReq = await fetch(`/api/users/auth`, {
-//         body: JSON.stringify({ publicAddress: account, signature }),
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         method: "POST",
-//       });
-//       if (!authReq.ok) throw new Error("Could not verify signature");
-
-//       // Call the callback with our wallet address
-//       onWalletSignature && (await onWalletSignature(account));
-
-//       // const network = await library.getNetwork();
-//       // setChainId(network.chainId);
-//     } catch (error) {
-//       disconnect();
-//     }
-//   };
-
-//   const disconnect = () => {
-//     wallet.clear();
-
-//     setAccount("");
-//     setSignature("");
-//   };
-
-//   return (
-//     <button
-//       onClick={account ? disconnect : connect}
-//       className="text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4"
-//     >
-//       {!account && !signature && "Connect wallet"}
-//       {account && !signature && "Waiting on signature"}
-//       {account && signature && "Disconnect"}
-//     </button>
-//   );
-// };
-
-// export default Wallet;
-
-import { useState, useEffect } from "react";
-import { message } from "@lib/utils";
+import { useState } from "react";
 import { walletUtil } from "@lib/wallet";
-import { Web3Provider } from "@coinbase/wallet-sdk/dist/provider/Web3Provider";
 
 const wallet = walletUtil();
 
 interface WalletProps {
   onWalletSignature?: (address: string) => Promise<void>;
 }
+
 const Wallet = ({ onWalletSignature }: WalletProps) => {
-  const [signature, setSignature] = useState<string>("");
-  // console.log("signature: ", signature);
-  const [userNonce, setUserNonce] = useState<any>();
-  // console.log("userNonce: ", userNonce);
-  const [userLibrary, setUserLibrary] = useState<any>();
-  // console.log("userLibrary: ", userLibrary);
-  const [userAccount, setUserAccount] = useState<any>();
-  // console.log("userAccount: ", userAccount);
+  const [userSignature, setUserSignature] = useState<string>("");
+  const [userAccount, setUserAccount] = useState<string>("");
 
   const connect = async (): Promise<void> => {
     try {
-      const { library, account } = await wallet.trigger();
-      console.log("account: ", account);
-
+      const { account } = await wallet.trigger();
       setUserAccount(account);
-      setUserLibrary(library);
-
-      // Fetch our nonce used to sign our message
-      const nonceReq = await fetch(`/api/users/${account}`);
-      const { nonce } = await nonceReq.json();
-      setUserNonce(nonce);
-
-      if (!library?.provider?.request) throw new Error("Library is undefined");
     } catch (error) {
-      console.error(error);
       disconnect();
     }
   };
 
-  const sign = async () => {
-    console.log("herbie hancock");
+  const sign = async (): Promise<void> => {
     try {
-      if (!userLibrary?.provider?.request)
-        throw new Error("Library is undefined");
-
-      const signature = await userLibrary.provider.request({
-        method: "personal_sign",
-        params: [message(userNonce), userAccount],
-      });
-
-      setSignature(signature);
-      // With public address + signature, we can verify the signature
-      const authReq = await fetch(`/api/users/auth`, {
-        body: JSON.stringify({ publicAddress: userAccount, signature }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
-      if (!authReq.ok) throw new Error("Could not verify signature");
+      const signature = await wallet.sign();
+      setUserSignature(signature);
 
       // Call the callback with our wallet address
       onWalletSignature && (await onWalletSignature(userAccount));
@@ -139,7 +31,6 @@ const Wallet = ({ onWalletSignature }: WalletProps) => {
       // const network = await library.getNetwork();
       // setChainId(network.chainId);
     } catch (error) {
-      console.error(error);
       disconnect();
     }
   };
@@ -148,23 +39,23 @@ const Wallet = ({ onWalletSignature }: WalletProps) => {
     wallet.clear();
 
     setUserAccount("");
-    setSignature("");
+    setUserSignature("");
   };
 
   return (
     <button
       onClick={
-        userAccount && !signature
+        userAccount && !userSignature
           ? sign
-          : userAccount && signature
+          : userAccount && userSignature
           ? disconnect
           : connect
       }
       className="text-blue font-bold bg-turquoise hover:bg-turquoiseDark rounded-md py-2 px-4"
     >
-      {!userAccount && !signature && "Connect wallet"}
-      {userAccount && !signature && "Sign"}
-      {userAccount && signature && "Disconnect"}
+      {!userAccount && !userSignature && "Connect wallet"}
+      {userAccount && !userSignature && "Sign"}
+      {userAccount && userSignature && "Disconnect"}
     </button>
   );
 };
