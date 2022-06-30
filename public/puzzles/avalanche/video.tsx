1cc4c616e9a7427d554f790ac66667abea337bab
Kori Riddick <entense@att.net>
2022-06-30 14:28:06 -0400

    adjust css & created global theme for aspect ratio

--- public/puzzles/avalanche/video.tsx
+++ public/puzzles/avalanche/video.tsx
@@ -6,21 +6,16 @@ interface VideoProps {
 }
 
 const Video: FC<VideoProps> = ({ src, title }) => (
-  <span className="container flex justify-center">
-    <span className="">
-      <iframe
-        className="aspect-video"
-        // height={409}
-        // width={727}
-        height="100%"
-        width="100%"
-        src={src}
-        title={title}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </span>
+  <span className="container block max-w-xl videoAspect">
+    <iframe
+      height="100%"
+      width="100%"
+      src={src}
+      title={title}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
   </span>
 );
 
