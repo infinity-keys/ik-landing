3cd30eb7af0e34c12ec9648eda03fc42d0bb9cbf
Christopher Bloom <bloomcb@gmail.com>
2022-07-01 11:39:38 -0700

    Middleware tune up

--- middleware.ts
+++ middleware.ts
@@ -11,27 +11,29 @@ export async function middleware(req: NextRequest) {
   let token = req.cookies.get(IK_ID_COOKIE);
 
   if (req.nextUrl.pathname.startsWith("/solved")) {
-    // No token is nonstarter
-    if (!token) return NextResponse.redirect(new URL("/", req.url));
-
-    // This doesn't work yet, not available before we need to redirect
-    // const success = req.nextUrl.searchParams.get("success");
-    // Instead we'll use this gross-ass hack
-    const success = req.nextUrl.pathname.split("/")[2];
-
-    // Bail if we're not on the solved page
-    if (!success) return NextResponse.redirect(new URL("/", req.url));
-
     try {
+      // No token is nonstarter
+      if (!token) throw new Error("No token on solved page");
+
+      // This doesn't work yet, not available before we need to redirect const
+      // success = req.nextUrl.searchParams.get("success"); Instead we'll use
+      // this gross-ass hack: /solved/name-of-puzzle
+      const success = req.nextUrl.pathname.split("/")[2];
+
+      // Bail if we're not on the solved page
+      if (!success) throw new Error('"success" param not found on solved page');
+
       const verified = await verifyToken(token);
+      // @TOOD: pull in superstruct or use jose's validator to ensure shape
       const payload = verified.payload as unknown as IkJwt;
 
       // Allow through if the user has this puzzle on their claims
       if (payload?.claims?.[IK_CLAIMS_NAMESPACE].puzzles.includes(success))
         return response;
       // Otherwise bail
-      return NextResponse.redirect(new URL("/", req.url));
+      throw new Error("User does not have solved puzzle claim");
     } catch (e) {
+      console.error(e);
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
