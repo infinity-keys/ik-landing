32b4f6a598055fc5f528eabbcf1d3e04127ff0d0
Tawnee <allprt89@gmail.com>
2022-07-11 08:39:44 -0600

    add styles and toggle between grid and list views

--- pages/puzzles.tsx
+++ pages/puzzles.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
-
+import clsx from "clsx";
 // import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
 
 import { gqlApiSdk } from "@lib/server";
@@ -15,6 +16,11 @@ interface PageProps {
 }
 
 const Puzzles: NextPage<PageProps> = ({ puzzles }) => {
+  const [isGrid, setIsGrid] = useState(true);
+  const gridlist =
+    "grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-8";
+  const gridlistitem = "col-span-1 divide-y divide-gray-200";
+
   return (
     <Wrapper>
       <Head>
@@ -23,21 +29,25 @@ const Puzzles: NextPage<PageProps> = ({ puzzles }) => {
       <div className="radial-bg scanlines min-h-screen flex flex-col">
         <Header />
 
-        <div className="container px-4 flex-1">
+        <div className="px-4 flex-1 container">
+          <button onClick={() => setIsGrid(!isGrid)}>toggle</button>
           <ul
             role="list"
-            className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-8"
+            className={clsx(
+              "grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 py-8",
+              { "lg:grid-cols-4": isGrid }
+            )}
           >
-            {puzzles.map(({ puzzle_id, landing_route, simple_name }) => (
-              <li
-                key={puzzle_id}
-                className="col-span-1 divide-y divide-gray-200"
-              >
-                <PuzzleThumbnail
-                  {...{ puzzle_id, landing_route, simple_name }}
-                />
-              </li>
-            ))}
+            {[...Array(50).keys()].map(
+              ({ puzzle_id, landing_route, simple_name }) => (
+                <li key={puzzle_id} className="">
+                  <PuzzleThumbnail
+                    grid={isGrid}
+                    {...{ puzzle_id, landing_route, simple_name }}
+                  />
+                </li>
+              )
+            )}
           </ul>
         </div>
 
