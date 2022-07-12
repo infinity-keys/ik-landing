16bcfab5d1c089154d19ca1956d3c4c62069cf2a
Tawnee <allprt89@gmail.com>
2022-07-12 11:23:39 -0600

    add localstorage for puzzle view

--- pages/puzzles.tsx
+++ pages/puzzles.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
 import clsx from "clsx";
@@ -16,7 +16,17 @@ interface PageProps {
 }
 
 const Puzzles: NextPage<PageProps> = ({ puzzles }) => {
-  const [isGrid, setIsGrid] = useState(true);
+  const [isGrid, setIsGrid] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const puzzlesView = window.localStorage.getItem("puzzlesView");
+    setIsGrid(puzzlesView !== null ? JSON.parse(puzzlesView) : true);
+  }, []);
+
+  const setView = ({ gridView }: { gridView: boolean }) => {
+    setIsGrid(gridView);
+    window.localStorage.setItem("puzzlesView", JSON.stringify(gridView));
+  };
 
   return (
     <Wrapper>
@@ -27,45 +37,53 @@ const Puzzles: NextPage<PageProps> = ({ puzzles }) => {
         <Header />
 
         <div className="px-4 flex-1 container">
-          <button
-            onClick={() => setIsGrid(true)}
-            aria-label="set grid view"
-            className={clsx(
-              "border mr-2 bg-white/10 p-2 rounded-md transition-all duration-200",
-              isGrid ? "border-white/20" : "border-transparent text-gray-400"
-            )}
-          >
-            <ViewGridIcon className="h-5 w-5" aria-hidden="true" />
-          </button>
-          <button
-            onClick={() => setIsGrid(false)}
-            aria-label="set list view"
-            className={clsx(
-              "border mt-8 bg-white/10 p-2 rounded-md transition-all duration-200	",
-              !isGrid ? "border-white/20" : "border-transparent text-gray-400"
-            )}
-          >
-            <ViewListIcon className="h-5 w-5" aria-hidden="true" />
-          </button>
+          {isGrid !== null && (
+            <>
+              <button
+                onClick={() => setView({ gridView: true })}
+                aria-label="set grid view"
+                className={clsx(
+                  "border mr-2 bg-white/10 p-2 rounded-md transition-all duration-200",
+                  isGrid
+                    ? "border-white/20"
+                    : "border-transparent text-gray-400"
+                )}
+              >
+                <ViewGridIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+              <button
+                onClick={() => setView({ gridView: false })}
+                aria-label="set list view"
+                className={clsx(
+                  "border mt-8 bg-white/10 p-2 rounded-md transition-all duration-200	",
+                  !isGrid
+                    ? "border-white/20"
+                    : "border-transparent text-gray-400"
+                )}
+              >
+                <ViewListIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
 
-          <ul
-            role="list"
-            className={clsx(
-              "grid grid-cols-1 gap-6 py-8 sm:grid-cols-2",
-              isGrid
-                ? "md:grid-cols-3 lg:grid-cols-4"
-                : "lg:grid-cols-3 xl:grid-cols-4"
-            )}
-          >
-            {puzzles.map(({ puzzle_id, landing_route, simple_name }) => (
-              <li key={puzzle_id} className="">
-                <PuzzleThumbnail
-                  isGrid={isGrid}
-                  {...{ puzzle_id, landing_route, simple_name }}
-                />
-              </li>
-            ))}
-          </ul>
+              <ul
+                role="list"
+                className={clsx(
+                  "grid grid-cols-1 gap-6 py-8 sm:grid-cols-2",
+                  isGrid
+                    ? "md:grid-cols-3 lg:grid-cols-4"
+                    : "lg:grid-cols-3 xl:grid-cols-4"
+                )}
+              >
+                {puzzles.map(({ puzzle_id, landing_route, simple_name }) => (
+                  <li key={puzzle_id} className="">
+                    <PuzzleThumbnail
+                      isGrid={isGrid}
+                      {...{ puzzle_id, landing_route, simple_name }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
 
         <Footer />
