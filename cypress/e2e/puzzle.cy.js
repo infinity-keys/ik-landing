a772e5c52f1a3d9e31b6b9965c57505b34867bd5
Kori Riddick <entense@att.net>
2022-07-14 13:56:05 -0400

    another code fix

--- cypress/e2e/puzzle.cy.js
+++ cypress/e2e/puzzle.cy.js
@@ -11,12 +11,12 @@ describe("infinitykeys.io/puzzles", () => {
     cy.url().should("include", "/puzzles");
   });
 
-  it("should click on puzzle thumbnail and navigate to puzzle landing page", () => {
+  it.only("should click on puzzle thumbnail and navigate to puzzle landing page", () => {
     cy.visit("/");
     cy.get("a.play").contains("Puzzles").click();
     cy.url().should("include", "/puzzles");
-    //cy.get(".puzzle-thumb").first().click();
-    cy.get(".puzzle-thumb").click({ multiple: true });
+    cy.get(".puzzle-thumb").first().click();
+    // cy.get(".puzzle-thumb").first().click({ multiple: true });
     cy.url().should("include", "/puzzle");
   });
 });
