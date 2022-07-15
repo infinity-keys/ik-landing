b8128dca5d4781e6c6d01e7b77081cf8e8c83d93
Kori Riddick <entense@att.net>
2022-07-15 15:48:19 -0400

    create tests to check thumbnails and navigation

--- cypress/e2e/puzzle.cy.js
+++ cypress/e2e/puzzle.cy.js
@@ -1,5 +1,5 @@
 describe("infinitykeys.io/puzzles", () => {
-  it("should click button and navigate to puzzle dashboard", () => {
+  it("should click puzzle button and navigate to puzzle dashboard", () => {
     cy.visit("/");
     cy.contains("a.play", "Puzzles").click();
     cy.url().should("include", "/puzzles");
@@ -11,12 +11,19 @@ describe("infinitykeys.io/puzzles", () => {
     cy.url().should("include", "/puzzles");
   });
 
-  it.only("should click on puzzle thumbnail and navigate to puzzle landing page", () => {
+  it("should click on puzzle thumbnail and navigate to puzzle landing page", () => {
     cy.visit("/");
     cy.get("a.play").contains("Puzzles").click();
     cy.url().should("include", "/puzzles");
-    cy.get(".puzzle-thumb").first().click();
-    // cy.get(".puzzle-thumb").first().click({ multiple: true });
+    cy.get(".puzzle-thumb").contains("thesis").click();
+    cy.url().should("include", "/puzzle");
+  });
+
+  it.only("should click on multiple thumbnails and navigate to select puzzle landing pages", () => {
+    cy.visit("/");
+    cy.contains("Hunts").click();
+    cy.get(".puzzle-thumb").click({ multiple: true });
+    cy.get(".puzzle-thumb").contains("avalanche").click();
     cy.url().should("include", "/puzzle");
   });
 });
