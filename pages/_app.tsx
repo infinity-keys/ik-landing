fcf70d93891d74839b417c6e5118885908cc83bf
Christopher Bloom <bloomcb@gmail.com>
2022-07-19 09:55:44 -0700

    Pop stately only for dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from "next/app";
 import { inspect } from '@xstate/inspect'
 
+const { NODE_ENV } = process.env
+
 // Re-enable here and below for auth via auth0
 // import { UserProvider } from "@auth0/nextjs-auth0";
 
 import "loaders.css/loaders.min.css";
 import "../styles/globals.css";
 
-if (typeof window !== "undefined") {
+if (typeof window !== "undefined" && NODE_ENV === "development") {
   inspect({
     iframe: false // open in new window
   });
