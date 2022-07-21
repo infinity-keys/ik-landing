37aa17c41fea6511fa7ce3e3d80ad3005eeded4b
Kori Riddick <entense@att.net>
2022-07-21 11:58:24 -0400

    code cleanup

--- cypress/e2e/puzzle.cy.js
+++ cypress/e2e/puzzle.cy.js
@@ -4,29 +4,18 @@ beforeEach(() => {
 
 describe("infinitykeys.io/puzzles", () => {
   it("should click puzzle button and navigate to puzzle dashboard", () => {
-    // cy.visit("/");
     cy.contains("a.play", "Puzzles").click();
     cy.url().should("include", Cypress.config().baseUrl + "/puzzles");
     cy.go("back");
   });
 
   it("should click hunts link and navigate to puzzle dashboard", () => {
-    // cy.visit("/");
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.url().should("include", Cypress.config().baseUrl + "/puzzles");
     cy.go("back");
   });
 
-  // it("should click on puzzle thumbnail and navigate to puzzle landing page", () => {
-  // cy.visit("/");
-  //   cy.get("a.play").contains("Puzzles").click();
-  //   cy.url().should("include", "/puzzles");
-  //   cy.get(".puzzle-thumb").contains("thesis").click();
-  //   cy.url().should("include", Cypress.config().baseUrl + "/puzzle");
-  // });
-
   it("should click on multiple thumbnails", () => {
-    // cy.visit("/");
     cy.get("a.header-nav--link").contains("Hunts").click();
     cy.get(".puzzle-thumb").first().click();
     cy.get(".puzzle-thumb").last().click();
@@ -34,7 +23,6 @@ describe("infinitykeys.io/puzzles", () => {
   });
 
   it("go to landing page and verify input boxes", () => {
-    // cy.visit("/");
     cy.get("a.play").contains("Puzzles").click();
     cy.get(".puzzle-thumb").contains("notright").click();
     cy.url().should("include", "/puzzle/notright");
@@ -43,7 +31,6 @@ describe("infinitykeys.io/puzzles", () => {
   });
 
   it("go to landing page and verify input field", () => {
-    // cy.visit("/");
     cy.get("a.play").contains("Puzzles").click();
     cy.get(".puzzle-thumb").contains("communitycode").click();
     cy.url().should("include", "/puzzle/communitycode");
@@ -52,7 +39,6 @@ describe("infinitykeys.io/puzzles", () => {
   });
 
   it("should click on next and previous buttons", () => {
-    // cy.visit("/");
     cy.get("a.play").contains("Puzzles").click();
     cy.get("a.next").contains("Next").click();
     cy.get("a.previous").contains("Previous").click();
@@ -65,30 +51,30 @@ describe("infinitykeys.io/puzzles", () => {
     cy.get(".ik-code-input").eq(1).type("r", { delay: 250 });
     cy.get(".ik-code-input").eq(2).type("o", { delay: 250 });
     cy.get(".ik-code-input").eq(3).type("n", { delay: 250 });
-    cy.get(".ik-code-input").eq(4).type("g", { delay: 250 });
+    cy.get(".ik-code-input").eq(4).type("g", { delay: 350 });
     cy.url().should("include", "/solved/notright");
   });
 
-  it.only("incorrect input displays fail message", () => {
+  it("incorrect input displays fail message", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("notright").click();
     cy.get(".ik-code-input").eq(0).type("a", { delay: 250 });
     cy.get(".ik-code-input").eq(1).type("s", { delay: 250 });
     cy.get(".ik-code-input").eq(2).type("d", { delay: 250 });
     cy.get(".ik-code-input").eq(3).type("f", { delay: 250 });
-    cy.get(".ik-code-input").eq(4).type("h", { delay: 250 });
+    cy.get(".ik-code-input").eq(4).type("h", { delay: 350 });
     cy.url().should("include", "/puzzle/notright");
     cy.contains("Sorry, that answer was correct. Try wrong this time.");
   });
 
-  it.only("correct input navigates to puzzle solved page", () => {
+  it("correct input navigates to puzzle solved page", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("communitycode").click();
     cy.get("input").type("with kindness and respect");
     cy.url().should("include", "/solved/communitycode");
   });
 
-  it.only("incorrect input displays fail message", () => {
+  it("incorrect input displays fail message", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("communitycode").click();
     cy.get("input").type("thiw ssendink dna tcepser");
