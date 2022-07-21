b3a2e005640c0857ac52c623b45b605034e46351
Kori Riddick <entense@att.net>
2022-07-21 17:26:36 -0400

    update syntax on tests and code cleanup

--- cypress/e2e/puzzle.cy.js
+++ cypress/e2e/puzzle.cy.js
@@ -5,13 +5,13 @@ beforeEach(() => {
 describe("infinitykeys.io/puzzles", () => {
   it("should click puzzle button and navigate to puzzle dashboard", () => {
     cy.contains("a.play", "Puzzles").click();
-    cy.url().should("include", Cypress.config().baseUrl + "/puzzles");
+    cy.url().should("include", "/puzzles");
     cy.go("back");
   });
 
   it("should click hunts link and navigate to puzzle dashboard", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
-    cy.url().should("include", Cypress.config().baseUrl + "/puzzles");
+    cy.url().should("include", "/puzzles");
     cy.go("back");
   });
 
@@ -44,37 +44,36 @@ describe("infinitykeys.io/puzzles", () => {
     cy.get("a.previous").contains("Previous").click();
   });
 
-  it.only("correct input navigates to puzzle solved page", () => {
+  it("correct input using input boxes navigates to puzzle solved page", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("notright").click();
     cy.get(".ik-code-input").eq(0).type("w", { delay: 250 });
     cy.get(".ik-code-input").eq(1).type("r", { delay: 250 });
     cy.get(".ik-code-input").eq(2).type("o", { delay: 250 });
     cy.get(".ik-code-input").eq(3).type("n", { delay: 250 });
-    cy.get(".ik-code-input").eq(4).type("g", { delay: 350 });
+    cy.get(".ik-code-input").eq(4).type("g");
     cy.url().should("include", "/solved/notright");
   });
 
-  it("incorrect input displays fail message", () => {
+  it("incorrect input using input boxes displays fail message", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("notright").click();
     cy.get(".ik-code-input").eq(0).type("a", { delay: 250 });
     cy.get(".ik-code-input").eq(1).type("s", { delay: 250 });
     cy.get(".ik-code-input").eq(2).type("d", { delay: 250 });
     cy.get(".ik-code-input").eq(3).type("f", { delay: 250 });
-    cy.get(".ik-code-input").eq(4).type("h", { delay: 350 });
-    cy.url().should("include", "/puzzle/notright");
+    cy.get(".ik-code-input").eq(4).type("h", { delay: 250 });
     cy.contains("Sorry, that answer was correct. Try wrong this time.");
   });
 
-  it("correct input navigates to puzzle solved page", () => {
+  it("correct input using input field navigates to puzzle solved page", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("communitycode").click();
     cy.get("input").type("with kindness and respect");
     cy.url().should("include", "/solved/communitycode");
   });
 
-  it("incorrect input displays fail message", () => {
+  it("incorrect input using input field displays fail message", () => {
     cy.contains("a.header-nav--link", "Hunts").click();
     cy.get(".puzzle-thumb").contains("communitycode").click();
     cy.get("input").type("thiw ssendink dna tcepser");
