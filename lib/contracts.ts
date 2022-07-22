3cfa153132f7fcb216408219558336e52bec90b1
Christopher Bloom <bloomcb@gmail.com>
2022-07-22 15:19:12 -0700

    contract with local cache

---
+++ lib/contracts.ts
@@ -0,0 +1,21 @@
+import { IKAchievementABI__factory } from "@contracts/factories/IKAchievementABI__factory";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS_POLYGON, POLYGON_RPC } from "./constants";
+
+export let contractPolygon:
+  | undefined
+  | ReturnType<typeof IKAchievementABI__factory.connect> = undefined;
+
+const getcontractPolygon = () => {
+  if (contractPolygon) return contractPolygon;
+
+  contractPolygon = IKAchievementABI__factory.connect(
+    CONTRACT_ADDRESS_POLYGON,
+    new ethers.providers.JsonRpcProvider(POLYGON_RPC)
+  );
+};
+
+// Use it like this:
+//
+// import {getcontractPolygon} from '@lib/contracts';
+// const contractPolygon = getcontractPolygon();
