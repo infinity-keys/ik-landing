51f8c61a87733b00655f4e955d411e36906bb882
hammm.eth <38670367+ham-evans@users.noreply.github.com>
2022-07-21 20:53:01 -0700

    ENG-32: web3 minting stuff (#115)
    * Added future Mint page, rough minting functionality
    
    * Integrating updated wallet connect package
    
    * setup basic page styles
    
    * add dummy flow
    
    * cleanup conditionals and classes. add secondary button
    
    * Added minter + transaction util, implementing mint functionality
    
    * issues with ending the tx processing circle
    
    * Fixed infinite spinning bugs, added marketplace links
    
    * adding tx messages, connect wallet loading
    
    * Getting rid of state and effects for minter.ts and transactions.ts
    
    * Rename files
    
    * signature return for minting verification
    
    * created new-claim and new-minter to help cleanup transaction flow
    
    * Moving switch chain, updating claim flow
    
    * fixing ui updating bug
    
    * Minting functional, fixing up small ui issues
    
    * moving check if claimed flow to api
    
    * cleaning up constants, refactoring switchChain
    
    * updating puzzle
    
    * Move contract json to typechain factory
    
    * code cleanup
    
    * Code cleanup
    
    * Files in correct places, updating
    
    * moved contracts to nfts/contracts
    
    * minter updates
    
    * type verify return
    
    * change puzzleId => tokenId
    
    * new contracts testing
    
    * small updates
    
    * dark mode broken
    
    * URL strings
    
    * Secretes in GH for minting
    
    * Gate /claim, static props/paths for /claim
    
    * tweaks
    
    * final ABI added
    
    * Tune up and optimization
    
    * JWT verification at minting routes
    
    * Fix empty check for vercel
    
    * Remove JWT auth on achievements api route
    
    * ham/eng 172 web3 polygon addition (#176)
    
    * polygon constants added
    
    * polygon added to web3
    
    * reformatting buttons
    
    Co-authored-by: Christopher Bloom <bloomcb@gmail.com>
    
    * note on loader
    
    * constants updated for mainnet
    
    Co-authored-by: Ham <ham@infinitykeys.io>
    Co-authored-by: Tawnee <allprt89@gmail.com>
    Co-authored-by: Christopher Bloom <bloomcb@gmail.com>

--- lib/constants.ts
+++ lib/constants.ts
@@ -13,3 +13,46 @@ export const PUZZLE_SUCCESS_BASE = "solved";
 export const PUZZLE_FAILED_BASE = "puzzle"; // back to landing
 
 export const PAGINATION_COUNTS = [8, 16, 32, 64];
+
+//Minting Stuff
+//AVAX
+export const AVAX_CHAIN_ID = 43114;
+export const CONTRACT_ADDRESS_AVAX =
+  "0xB40fD6825a366081192d890d2760113C066761Ef";
+export const SNOWTRACE_TRACKER = "https://snowtrace.io";
+export const AVAX_RPC = "https://api.avax.network/ext/bc/C/rpc";
+export const AVAX_MARKETPLACE_LINK = "https://joepegs.com/item/";
+export const joePegsLink = `${AVAX_MARKETPLACE_LINK}${CONTRACT_ADDRESS_AVAX}/`;
+export const AVAX_PARAMS = {
+  chainId: AVAX_CHAIN_ID,
+  rpcUrls: [AVAX_RPC],
+  chainName: "Avalanche Network",
+  nativeCurrency: { name: "AVAX", decimals: 18, symbol: "AVAX" },
+  blockExplorerUrls: [SNOWTRACE_TRACKER],
+};
+
+// ETH PARAMS
+export const ETH_CHAIN_ID = 1;
+export const CONTRACT_ADDRESS_ETH =
+  "0x54b743D6055e3BBBF13eb2C748A3783516156e5B";
+export const ETHERSCAN_TRACKER = "https://etherscan.io";
+export const ETH_RPC_ID = "c10d222a5bae4a8e97fad0915b06ff5d";
+export const ETH_RPC = `https://mainnet.infura.io/v3/${ETH_RPC_ID}`;
+export const ETH_MARKETPLACE_LINK = "https://opensea.io/assets/ethereum/";
+export const openseaLink = `${ETH_MARKETPLACE_LINK}${CONTRACT_ADDRESS_ETH}/`;
+
+// POLYGON PARAMS
+export const POLYGON_CHAIN_ID = 137;
+export const CONTRACT_ADDRESS_POLYGON =
+  "0x7e8E97A66A935061B2f5a8576226175c4fdE0ff9";
+export const POLYGONSCAN_TRACKER = "https://polygonscan.com/";
+export const POLYGON_RPC = "https://polygon-rpc.com";
+export const POLYGON_MARKETPLACE_LINK = "https://opensea.io/assets/matic/";
+export const openseaPolygonLink = `${POLYGON_MARKETPLACE_LINK}${CONTRACT_ADDRESS_POLYGON}/`;
+export const POLYGON_PARAMS = {
+  chainId: `0x${POLYGON_CHAIN_ID}`,
+  rpcUrls: [{ POLYGON_RPC }],
+  chainName: "Polygon",
+  nativeCurrency: { name: "MATIC", decimals: 18, symbol: "MATIC" },
+  blockExplorerUrls: [{ POLYGONSCAN_TRACKER }],
+};
