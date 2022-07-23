72da797e7c2dd8de2e61830d6e28f933f0971bc1
Tawnee <allprt89@gmail.com>
2022-07-22 21:10:23 -0600

    add query, add starterpack page, adjust middlware

--- middleware.ts
+++ middleware.ts
@@ -24,7 +24,9 @@ export async function middleware(req: NextRequest) {
       // Bail if we're not on the solved page
       if (!success) throw new Error('"success" param not found on gated page');
 
-      const allowedToView = await jwtHasClaim(token, [success]);
+      const allowedToView =
+        (await jwtHasClaim(token, [success])) ||
+        req.nextUrl.pathname.startsWith("/claim/starterpack");
 
       if (allowedToView) return response;
       // Otherwise bail
