5e0cd818544de623a2cd861663d725e7eb449b0b
Tawnee <allprt89@gmail.com>
2022-07-24 17:59:46 -0600

    add general layout

--- pages/claim/starterpack.tsx
+++ pages/claim/starterpack.tsx
@@ -1,28 +1,86 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import clsx from "clsx";
 
 import { gqlApiSdk } from "@lib/server";
 // @TODO: remove when updated with Ham's layout
 import Header from "@components/header";
 import Footer from "@components/footer";
 import Wrapper from "@components/wrapper";
+import PuzzleThumbnail from "@components/puzzle-thumbnail";
 import { StarterPackPuzzlesQuery } from "@lib/generated/graphql";
+import { PuzzleLayoutType } from "@lib/types";
 
 interface PageProps {
   puzzles: StarterPackPuzzlesQuery["puzzles"];
 }
 
+const buttonData = [
+  {
+    // chain_id: ETH_CHAIN_ID,
+    name: "Ethereum",
+  },
+  {
+    // chain_id: AVAX_CHAIN_ID,
+    name: "Avalanche",
+  },
+  {
+    // chain_id: POLYGON_CHAIN_ID,
+    name: "Polygon",
+  },
+];
+
 const StarterPack: NextPage<PageProps> = ({ puzzles }) => {
+  const layout = PuzzleLayoutType.Grid;
+
   return (
     <Wrapper>
       <Head>
-        <title>Starter Pack - Infinity Keys</title>
+        <title>Infinity Keys</title>
       </Head>
       <Header />
 
       <div className="w-full pt-4 pb-4 min-h-screen radial-bg relative z-0">
-        <div className="container">
-          <p>Check back soon</p>
+        <div className="container px-4 max-w-3xl">
+          <p className="mt-12">
+            To be eligible to claim the Starter Pack Achievement you must
+            successfully complete the following puzzles and claim the
+            corresponding achievement NFT. All three NFTs should be claimed on
+            the same chain to qualify.
+          </p>
+
+          <ul
+            role="list"
+            className={clsx(
+              "grid grid-cols-1 gap-6 py-8 sm:grid-cols-2 md:grid-cols-3 my-14"
+            )}
+          >
+            {puzzles.map(({ puzzle_id, landing_route, simple_name }) => (
+              <li key={puzzle_id}>
+                <PuzzleThumbnail
+                  isGrid={layout === PuzzleLayoutType.Grid}
+                  {...{ puzzle_id, landing_route, simple_name }}
+                />
+              </li>
+            ))}
+          </ul>
+
+          <div className="text-center">
+            <button className="text-sm text-blue font-bold bg-turquoise border-solid border-2 border-turquoise hover:bg-turquoiseDark rounded-md py-2 w-44 mb-8">
+              Check Wallet
+            </button>
+
+            <div className="text-white/75 flex flex-col md:block">
+              {buttonData.map(({ name }) => (
+                <button
+                  className="transition my-2 hover:text-turquoise md:mx-4 md:my-0"
+                  key={name}
+                >
+                  Switch to {name}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
