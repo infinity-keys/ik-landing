833f96316a70872215a1426557e003687adaee32
Ham <ham97@mac.com>
2022-07-25 18:09:46 -0700

    rinkeby update

--- pages/api/metadata/achievement.ts
+++ pages/api/metadata/achievement.ts
@@ -15,11 +15,15 @@ export default async function handler(
   const nftId = parseInt(tokenid, 10);
 
   const contractPolygon = IKAchievementABI__factory.connect(
-    CONTRACT_ADDRESS_POLYGON,
-    new ethers.providers.JsonRpcProvider(POLYGON_RPC)
+    // CONTRACT_ADDRESS_POLYGON,
+    // new ethers.providers.JsonRpcProvider(POLYGON_RPC)
+    "0xbcebf2f7f6d23287054008aeb028f2092262d1a3",
+    new ethers.providers.JsonRpcProvider(
+      "https://rinkeby.infura.io/v3/c10d222a5bae4a8e97fad0915b06ff5d"
+    )
   );
   const numTokens = (await contractPolygon.totalSupplyAll()).length;
-  //if (nftId >= numTokens) return res.status(500).end();
+  if (nftId >= numTokens) return res.status(500).end();
 
   const gql = await gqlApiSdk();
   const { nft_metadata_by_pk } = await gql.NftMetadata({
