d4446ca59f356e77a838ef3f0d2a125e12f5a966
hammm.eth <38670367+ham-evans@users.noreply.github.com>
2022-07-27 14:34:38 -0700

    ham/eng 156 metadata api (#157)
    * metadata api and metadata json
    
    * updating metadata
    
    * puzzleId => tokenId
    
    * api grabbing database data
    
    * deleting unused
    
    * for testing
    
    * rinkeby update
    
    * done testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
-import { inspect } from '@xstate/inspect'
+import { inspect } from "@xstate/inspect";
 
-const { NODE_ENV } = process.env
+const { NODE_ENV } = process.env;
 
 // Re-enable here and below for auth via auth0
 // import { UserProvider } from "@auth0/nextjs-auth0";
@@ -11,9 +11,8 @@ import "../styles/globals.css";
 
 if (typeof window !== "undefined" && NODE_ENV === "development") {
   inspect({
-    iframe: false // open in new window
+    iframe: false, // open in new window
   });
-
 }
 
 function InfinityKeysApp({ Component, pageProps }: AppProps) {
