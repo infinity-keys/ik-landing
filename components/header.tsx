99d3b350e722840afbb4b9b1346eaf10f7e5631b
Tawnee <allprt89@gmail.com>
2022-07-29 10:06:57 -0600

    readd flex

--- components/header.tsx
+++ components/header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
                   </Link>
                 </div>
               </div>
-              <div className="menu-items items-center justify-center sm:items-stretch sm:justify-start hidden lg:block">
+              <div className="menu-items items-center justify-center sm:items-stretch sm:justify-start hidden lg:flex">
                 <div className="sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
