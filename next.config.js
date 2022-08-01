a59da9654c7c9062a2a28ac48630967ddfc93d43
chair28980 <vrycmfy@protonmail.com>
2022-08-01 13:20:05 -0700

    update default puzzle count

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
       // Forward old avalanche page to new
       { source: "/avalanche", destination: "/puzzle/avalanche" },
       // Alias default puzzle count and page to /puzzles
-      { source: "/puzzles/8/1", destination: "/puzzles" },
+      { source: "/puzzles/16/1", destination: "/puzzles" },
     ];
   },
 };
