ab474d685cc1f129908c864b290635fd24b6eabc
Tawnee <allprt89@gmail.com>
2022-08-02 17:28:55 -0600

    use constants

--- lib/images.ts
+++ lib/images.ts
@@ -1,9 +1,10 @@
 import { buildUrl } from "cloudinary-build-url";
+import { CLOUDINARY_CLOUD_NAME } from "@lib/constants";
 
 export const cloudinaryUrl = (id: string, height: number, width: number) => {
   return buildUrl(id, {
     cloud: {
-      cloudName: "infinity-keys",
+      cloudName: CLOUDINARY_CLOUD_NAME,
     },
     transformations: {
       resize: {
