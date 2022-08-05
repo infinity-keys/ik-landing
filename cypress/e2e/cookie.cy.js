69cf05ab521a70ab91004efa5a5bb7909120b4cf
Kori Riddick <entense@att.net>
2022-08-05 16:10:51 -0400

    update syntax to retrieve cookie

--- cypress/e2e/cookie.cy.js
+++ cypress/e2e/cookie.cy.js
@@ -4,10 +4,10 @@
 
 describe("infinitykeys.io", () => {
   it("display all cookies", () => {
-    cy.contains("a.header-nav--link", "Hunts").click();
-    cy.get(".puzzle-thumb").contains("notright").click();
-    cy.get(".ik-code-input").first().wait(1000).type("wrong", { delay: 750 });
-    cy.url().should("include", "/solved/notright");
-    cy.getCookies();
+    // cy.contains("a.header-nav--link", "Hunts").click();
+    // cy.get(".puzzle-thumb").contains("notright").click();
+    // cy.get(".ik-code-input").first().wait(1000).type("wrong", { delay: 750 });
+    // cy.url().should("include", "/solved/notright");
+    cy.getCookies("_ga");
   });
 });
