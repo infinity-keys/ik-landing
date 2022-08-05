af48700e58de0a75c871abde3cefabb8e33aa99c
Kori Riddick <entense@att.net>
2022-08-05 11:48:20 -0400

    create new test file and test to retrieve cookies

---
+++ cypress/e2e/cookie.cy.js
@@ -0,0 +1,13 @@
+// beforeEach(() => {
+//   cy.visit("/");
+// });
+
+describe("infinitykeys.io", () => {
+  it("display all cookies", () => {
+    cy.contains("a.header-nav--link", "Hunts").click();
+    cy.get(".puzzle-thumb").contains("notright").click();
+    cy.get(".ik-code-input").first().wait(1000).type("wrong", { delay: 750 });
+    cy.url().should("include", "/solved/notright");
+    cy.getCookies();
+  });
+});
