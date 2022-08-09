bc943cef322d427bc087b4af11ccbca366943df9
Tawnee <allprt89@gmail.com>
2022-08-09 11:30:55 -0600

    add typing to section component

--- components/section.tsx
+++ components/section.tsx
@@ -1,8 +1,6 @@
-import clsx from "clsx";
+import { HTMLProps } from "react";
 
-interface SectionProps {}
-
-export default function Section({ children, ...rest }: SectionProps) {
+export default function Section({ children, ...rest }: HTMLProps<HTMLElement>) {
   return (
     <section
       className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-28 lg:py-40"
