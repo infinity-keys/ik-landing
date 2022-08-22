c973db21424d862f5897617099ad56fc5b703bea
Kori Riddick <entense@att.net>
2022-08-22 18:31:22 -0400

    update test to show sub/puzzle attr & code cleanup

--- cypress/e2e/cookie.cy.ts
+++ cypress/e2e/cookie.cy.ts
@@ -2,31 +2,14 @@ import { decodeJwt } from "jose";
 
 beforeEach(() => {
   cy.visit("/");
+  cy.get('[data-cy="puzzle-link"]').contains("Puzzles").click();
 });
 
-describe("reads cookies in cypress", () => {
-  it.only("test if cookie exist and verify/decode jwt", () => {
-    cy.getCookie("ik-id")
-      .should("have.property", "value")
-      .then((cookie) => {
-        console.log(cookie);
-        //console.log(process.env.NODE_ENV);
-        if (!cookie) {
-          throw new Error("no cookie");
-        }
-        const decoded = decodeJwt(String(cookie));
-        console.log(decoded);
-      });
-  });
-
+describe("read cookies in cypress", () => {
   it("test if cookie exist when no puzzle attempt/puzzle fail/puzzle solve", () => {
-    cy.visit("/");
-    cy.get('[data-cy="puzzle-link"]').contains("Puzzles").click();
     cy.getCookie("ik-id")
       .should("have.property", "value")
       .then((cookie) => {
-        console.log(cookie);
-        console.log(process.env.NODE_ENV);
         if (!cookie) {
           throw new Error("no cookie");
         }
@@ -34,38 +17,33 @@ describe("reads cookies in cypress", () => {
         console.log(decoded);
       });
 
-    cy.visit("/");
-    cy.get('[data-cy="puzzle-link"]').contains("Puzzles").click();
     cy.get(".puzzle-thumb").contains("notright").click();
     cy.get(".ik-code-input").first().wait(1000).type("gnorw", { delay: 750 });
-    cy.wait(4000);
     cy.getCookie("ik-id")
       .should("have.property", "value")
       .then((cookie) => {
-        console.log(cookie);
-        console.log(process.env.NODE_ENV);
         if (!cookie) {
           throw new Error("no cookie");
         }
         const decoded = decodeJwt(String(cookie));
-        console.log(decoded);
+        cy.wrap(decoded.sub).as("userId");
+        cy.get("@userId").should("have.property", "sub");
+        cy.wrap(decoded.claims["https://infinitykeys.io"]).as("puzzleCount");
+        cy.get("@puzzleCount").should("have.property", "puzzles");
       });
 
-    cy.visit("/");
-    cy.get('[data-cy="puzzle-link"]').contains("Puzzles").click();
-    cy.get(".puzzle-thumb").contains("notright").click();
     cy.get(".ik-code-input").first().wait(1000).type("wrong", { delay: 750 });
-    cy.wait(4000);
     cy.getCookie("ik-id")
       .should("have.property", "value")
       .then((cookie) => {
-        console.log(cookie);
-        console.log(process.env.NODE_ENV);
         if (!cookie) {
           throw new Error("no cookie");
         }
         const decoded = decodeJwt(String(cookie));
-        console.log(decoded);
+        cy.wrap(decoded.sub).as("userId");
+        cy.wrap(decoded.claims["https://infinitykeys.io"]).as("puzzleCount");
+        cy.get("@userId").should("have.property", "sub");
+        cy.get("@puzzleCount").should("have.property", "puzzles");
       });
   });
 });
