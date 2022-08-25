a249983829f6750601e36e0ed5facfe8deaaef33
Tawnee <allprt89@gmail.com>
2022-08-25 14:18:31 -0600

    remove logs, enable sendgrid

--- pages/api/email-submission.ts
+++ pages/api/email-submission.ts
@@ -13,24 +13,20 @@ export default async function handler(
   if (req.method !== "POST") return res.status(405).end();
 
   const email = req.body.event?.data?.new?.form_data?.email || undefined;
-  console.log("email: ", email);
-  console.log("req.body: ", req.body);
-  console.log("form: ", req.body.event?.data?.new?.form_data);
-  console.log("id: ", req.body.event?.data?.new?.user_id);
 
   if (!email || email.trim().length <= 5 || !email.includes("@"))
     return res.status(400).json({ error: "Invalid email" });
 
-  // try {
-  //   await sendgrid.send({
-  //     to: email, // the recipient's email
-  //     from: "social@infinitykeys.io", // the SendGrid's sender email
-  //     subject: "It worked!",
-  //     html: `<div>You've got mail</div>`,
-  //   });
-  // } catch (error) {
-  //   return res.status(500).json({ error: "Error sending email" });
-  // }
+  try {
+    await sendgrid.send({
+      to: email, // the recipient's email
+      from: "social@infinitykeys.io", // the SendGrid's sender email
+      subject: "It worked!",
+      html: `<div>You've got mail</div>`,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Error sending email" });
+  }
 
   return res.status(200).json({ error: "" });
 }
