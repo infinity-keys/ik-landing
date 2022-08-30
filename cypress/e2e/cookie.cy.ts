69fdf6e9506fefb31c9d84c14fb58aedab042469
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2022-08-29 23:48:06 -0400

    Itsmekori/eng 216 in cypress get the jwt from the ik cookie (#243)
    * create new test file and test to retrieve cookies
    
    * update syntax to retrieve cookie
    
    * created new test file & test to find & decode jwt
    
    * test cookie when puzzle solve, fail & no attempt
    
    * update test to show sub/puzzle attr & code cleanup
    
    * resolve ts error and code cleanup
    
    * adjust global wait time
    
    * add code w/ sub & puzzle arr check per review note
    
    * add code to check for puzzle array value
    
    * code cleanup
    
    * Tune up cypress cookie test
    
    Co-authored-by: Christopher Bloom <bloomcb@gmail.com>

---
+++ cypress/e2e/cookie.cy.ts
@@ -0,0 +1,55 @@
+import { use } from "chai";
+import { decodeJwt } from "jose";
+import { IkJwt } from "../../lib/types";
+
+beforeEach(() => {
+  cy.visit("/puzzle/notright");
+});
+
+describe("read cookies in cypress", () => {
+  let userId: string | undefined;
+  let puzzlesClaims: string[];
+
+  it("claims start empty, puzzle solve adds claim. user stays consistent through session", () => {
+    cy.getCookie("ik-id")
+      .should("have.property", "value")
+      .then((cookie) => {
+        if (!cookie) {
+          throw new Error("no cookie");
+        }
+        const ikDecoded = decodeJwt(String(cookie)) as unknown as IkJwt;
+        userId = ikDecoded.sub;
+        puzzlesClaims = ikDecoded.claims["https://infinitykeys.io"].puzzles;
+
+        expect(userId).to.be.a("string");
+        expect(puzzlesClaims).to.be.empty;
+      });
+
+    cy.get(".ik-code-input").first().wait(1000).type("gnorw", { delay: 750 });
+    cy.getCookie("ik-id")
+      .should("have.property", "value")
+      .then((cookie) => {
+        if (!cookie) {
+          throw new Error("no cookie");
+        }
+        const ikDecoded = decodeJwt(String(cookie)) as unknown as IkJwt;
+        expect(ikDecoded.sub).equals(userId);
+        expect(puzzlesClaims).to.be.empty;
+      });
+
+    cy.get(".ik-code-input").first().wait(1000).type("wrong", { delay: 750 });
+    cy.getCookie("ik-id")
+      .should("have.property", "value")
+      .then((cookie) => {
+        if (!cookie) {
+          throw new Error("no cookie");
+        }
+        const ikDecoded = decodeJwt(String(cookie)) as unknown as IkJwt;
+        puzzlesClaims = ikDecoded.claims["https://infinitykeys.io"].puzzles;
+        expect(ikDecoded.sub).equals(userId);
+        expect(puzzlesClaims).to.include("notright");
+      });
+  });
+});
+
+export {};
