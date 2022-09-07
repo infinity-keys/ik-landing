4eb2ad99dd05963528af7b7ba5dca74753cf6e41
Kori Riddick <entense@att.net>
2022-09-07 15:55:40 -0400

    add comment for chain icons

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // chain icons
   images: {
     domains: [
       "images.unsplash.com",
