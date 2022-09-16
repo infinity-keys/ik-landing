4938db4cad0e595c70e57d8f8477b64aaac8271d
Tawnee <allprt89@gmail.com>
2022-09-16 08:16:13 -0600

    remove readfilesync, add helper function

--- scripts/import.mjs
+++ scripts/import.mjs
@@ -52,9 +52,6 @@ const doIt = async () => {
     }
   );
   const getResults = await getPuzzles.json();
-  console.log(
-    getResults.data.puzzles.find((puzzle) => puzzle.simple_name === "notright")
-  );
 
   // Example: Make GraphQL calls
   //const res = await fetch("https://hasura-2v34.onrender.com/v1/graphql", {
@@ -81,69 +78,24 @@ const doIt = async () => {
   //     console.log(puzzles);
   //   }
   // );
-  const updatePuzzles = async ({
-    simple_name,
-    challenge,
-    input_type,
-    landing_message,
-    solution,
-    fail_message,
-    success_message,
-  }) => {
-    const testRes = await fetch("https://hasura-2v34.onrender.com/v1/graphql", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
-      },
-      body: JSON.stringify({
-        operationName: "MyMutation",
-        query: gqlModify,
-        variables: {
-          simple_name,
-          challenge,
-          input_type,
-          landing_message,
-          solution,
-          fail_message,
-          success_message,
-        },
-      }),
-    });
-    const testResults = await testRes.json();
-    return testResults;
+
+  const updatePuzzles = async (variables) => {
+    console.log(variables);
+    // const testRes = await fetch("https://hasura-2v34.onrender.com/v1/graphql", {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //     "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+    //   },
+    //   body: JSON.stringify({
+    //     operationName: "MyMutation",
+    //     query: gqlModify,
+    //     variables,
+    //   }),
+    // });
+    // const testResults = await testRes.json();
+    // return testResults;
   };
-
-  const data = fs.readFileSync(
-    new URL("./updatedcsvjson.json", import.meta.url).pathname
-  );
-  const puzzles = JSON.parse(data);
-  const promises = puzzles.map((change) => {
-    const oldPuzzle = getResults.data.puzzles.find(
-      (puzzle) => puzzle.simple_name === String(change.simple_name)
-    );
-    if (change.simple_name === "testMutation") {
-      updatePuzzles({
-        simple_name: String(change.simple_name),
-        challenge: String(change.Challenge),
-        input_type: "boxes",
-        landing_message: String(change["Instructions (old landing_message)"]),
-        solution: String(change["Input (Passcode)"])
-          ? String(change["Input (Passcode)"])
-          : oldPuzzle.solution,
-        fail_message: String(change["Fail message"])
-          ? String(change["Fail message"])
-          : oldPuzzle.fail_message,
-        success_message: String(change["Success Message"])
-          ? String(change["Success Message"])
-          : oldPuzzle.success_message,
-      });
-    }
-  });
-
-  Promise.all(promises).then((values) => {
-    console.log(values);
-  });
 
   // Test: mutation to update input boxes
   //   const testRes = await fetch("https://hasura-2v34.onrender.com/v1/graphql", {
@@ -161,37 +113,46 @@ const doIt = async () => {
   //   const testResults = await testRes.json();
   //   console.log("test results", testResults.data.update_puzzles.returning);
 
-  // fs.readFile(
-  //   new URL("./updatedcsvjson.json", import.meta.url).pathname,
-  //   (_err, data) => {
-  //     const changes = JSON.parse(data);
-  //     changes.forEach((change) => {
-  //       const oldPuzzle = getResults.data.puzzles.find(
-  //         (puzzle) => puzzle.simple_name === String(change.simple_name)
-  //       );
-  //       if (String(change.simple_name) === "testMutation") {
-  //         console.log(change.simple_name);
-  //         updatePuzzles({
-  //           simple_name: String(change.simple_name),
-  //           challenge: String(change.Challenge),
-  //           input_type: "boxes",
-  //           landing_message: String(
-  //             change["Instructions (old landing_message)"]
-  //           ),
-  //           solution: String(change["Input (Passcode)"])
-  //             ? String(change["Input (Passcode)"])
-  //             : oldPuzzle.solution,
-  //           fail_message: String(change["Fail message"])
-  //             ? String(change["Fail message"])
-  //             : oldPuzzle.fail_message,
-  //           success_message: String(change["Success Message"])
-  //             ? String(change["Success Message"])
-  //             : oldPuzzle.success_message,
-  //         });
-  //       }
-  //     });
-  //   }
-  // );
+  const getGoodData = (newData, oldData) => {
+    return String(newData) ? String(newData) : oldData;
+  };
+
+  fs.readFile(
+    new URL("./updatedcsvjson.json", import.meta.url).pathname,
+    (_err, data) => {
+      const changes = JSON.parse(data);
+      changes.forEach((change) => {
+        const oldPuzzle = getResults.data.puzzles.find(
+          (puzzle) => puzzle.simple_name === String(change.simple_name)
+        );
+        if (
+          String(change.simple_name) === "testMutation" ||
+          String(change.simple_name) === "test2"
+        ) {
+          updatePuzzles({
+            simple_name: String(change.simple_name),
+            challenge: String(change.Challenge),
+            input_type: "boxes",
+            landing_message: String(
+              change["Instructions (old landing_message)"]
+            ),
+            solution: getGoodData(
+              change["Input (Passcode)"],
+              oldPuzzle.solution
+            ),
+            fail_message: getGoodData(
+              change["Fail message"],
+              oldPuzzle.fail_message
+            ),
+            success_message: getGoodData(
+              change["Success Message"],
+              oldPuzzle.success_message
+            ),
+          });
+        }
+      });
+    }
+  );
 };
 
 doIt();
