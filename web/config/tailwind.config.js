6e1ed9eac6b94b9ad9a561e331e51dca416992a0
Christopher Bloom <bloomcb@gmail.com>
2022-09-26 22:26:38 -0700

    Tailwind

---
+++ web/config/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+const tailwindforms = require("@tailwindcss/forms");
+
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        blue: '#101D42',
+        'blue-300': '#93c5fd',
+        'blue-800': '#070e1f',
+        turquoise: '#3FCCBB',
+        turquoiseDark: '#5ffae7',
+        twitterBlue: '#1da1f2',
+        discordPurple: '#5865f2',
+        gray: {
+          100: '#EEEFFC',
+          150: '#A6ADB4',
+          200: '#888FA1',
+          300: '#475171',
+          500: '#303B5B',
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [tailwindforms],
+}
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -6,14 +6,16 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <div className='container'>
+        <h1>HomePage</h1>
+        <p>
+          Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>home</code>, link to me with `
+          <Link to={routes.home()}>Home</Link>`
+        </p>
+      </div>
     </>
   )
 }
