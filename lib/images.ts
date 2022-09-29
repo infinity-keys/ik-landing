63133b6f12ccf13986125f4673d300f55c9ede35
Tawnee <allprt89@gmail.com>
2022-09-29 12:28:18 -0600

    add cloud image, and dpr package

---
+++ lib/images.ts
@@ -0,0 +1,28 @@
+import { buildUrl } from 'cloudinary-build-url'
+
+import { CLOUDINARY_CLOUD_NAME } from './constants'
+
+export const cloudinaryUrl = (
+  id: string,
+  height: number,
+  width: number,
+  circle: boolean,
+  dpr: number
+) => {
+  return buildUrl(id, {
+    cloud: {
+      cloudName: CLOUDINARY_CLOUD_NAME,
+    },
+    transformations: {
+      quality: '100',
+      radius: circle ? 'max' : 0,
+      format: 'png',
+      dpr,
+      resize: {
+        type: 'scale',
+        width,
+        height,
+      },
+    },
+  })
+}
---
+++ web/src/components/CloudImage/CloudImage.test.tsx
@@ -0,0 +1,14 @@
+import { render } from '@redwoodjs/testing/web'
+
+import CloudImage from './CloudImage'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
+
+describe('CloudImage', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<CloudImage />)
+    }).not.toThrow()
+  })
+})
