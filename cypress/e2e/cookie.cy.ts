01703828ba489baa7ea8762372dc1b577b1b02c4
Kori Riddick <entense@att.net>
2022-09-29 11:09:59 -0400

    created query to check for deleted users

--- cypress/e2e/cookie.cy.ts
+++ cypress/e2e/cookie.cy.ts
@@ -1,6 +1,7 @@
 import { decodeJwt } from "jose";
 import { IkJwt } from "../../lib/types";
 import { deleteUser } from "../../lib/fetchers";
+import { gqlApiSdk } from "@lib/server";
 import { makeUserToken } from "@lib/jwt";
 import { IK_CLAIMS_NAMESPACE } from "@lib/constants";
 
@@ -21,8 +22,21 @@ describe("read cookies in cypress", () => {
   });
 
   afterEach(() => {
-    cy.get("@userId").then(async (userId) => {
-      console.log(userId);
+    cy.get("@userJwt").then(async (userJwt) => {
+      console.log(userJwt);
+      try {
+        const ikDecoded = decodeJwt(String(userJwt)) as unknown as IkJwt;
+        const userId = ikDecoded.sub;
+        const gql = await gqlApiSdk();
+        const user = await gql.UserExist({ userId });
+        user.users_by_pk?.user_id;
+        if (user.users_by_pk?.user_id) {
+          await deleteUser(userJwt.toString());
+          cy.log(`user deleted ${userJwt}`);
+        }
+      } catch (error) {
+        cy.log(`user not deleted ${userJwt}, ${error}`);
+      }
       // const jwt = await generateUserDeleteJWT(userId.toString());
       // try {
       //   await deleteUser(jwt);
@@ -46,7 +60,10 @@ describe("read cookies in cypress", () => {
         }
         const ikDecoded = decodeJwt(String(cookie)) as unknown as IkJwt;
         userId = ikDecoded.sub;
+
         cy.wrap(userId).as("userId");
+        cy.wrap(String(cookie)).as("userJwt");
+
         puzzlesClaims = ikDecoded.claims["https://infinitykeys.io"].puzzles;
 
         expect(userId).to.be.a("string");
