c8835b857b6dad03bb3b1323def1059597dc5c31
Tawnee <allprt89@gmail.com>
2022-09-30 15:01:14 -0600

    add cookie consent, gtag types, scripts to html

---
+++ web/src/components/CookieConsentBanner/CookieConsentBanner.stories.tsx
@@ -0,0 +1,25 @@
+// When you've added props to your component,
+// pass Storybook's `args` through this story to control it from the addons panel:
+//
+// ```tsx
+// import type { ComponentStory } from '@storybook/react'
+//
+// export const generated: ComponentStory<typeof CookieConsent> = (args) => {
+//   return <CookieConsent {...args} />
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { ComponentMeta } from '@storybook/react'
+
+import CookieConsentBanner from './CookieConsentBanner'
+
+export const generated = () => {
+  return <CookieConsentBanner />
+}
+
+export default {
+  title: 'Components/CookieConsent',
+  component: CookieConsentBanner,
+} as ComponentMeta<typeof CookieConsentBanner>
