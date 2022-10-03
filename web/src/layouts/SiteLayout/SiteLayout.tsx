fffd094f2a2005e4769f6dc467920d6400a006a8
Tawnee <allprt89@gmail.com>
2022-10-03 15:50:14 -0600

    ENG-436: skip link (#436)
    add skip link to site layout

--- web/src/layouts/SiteLayout/SiteLayout.tsx
+++ web/src/layouts/SiteLayout/SiteLayout.tsx
@@ -1,5 +1,9 @@
+import { SkipNavLink, SkipNavContent } from '@redwoodjs/router'
+
 import Footer from 'src/components/Footer/Footer'
 import Header from 'src/components/Header/Header'
+
+import '@reach/skip-nav/styles.css'
 
 type SiteLayoutProps = {
   children?: React.ReactNode
@@ -8,8 +12,13 @@ type SiteLayoutProps = {
 const SiteLayout = ({ children }: SiteLayoutProps) => {
   return (
     <>
+      <div className="absolute top-0 left-0 z-[55]">
+        <SkipNavLink contentId="main-content" />
+      </div>
+
       <Header />
-      {children}
+      <SkipNavContent id="main-content" />
+      <main>{children}</main>
       <Footer />
     </>
   )
