7952e3c2447865799fbf54cf07c056623d9b2bfc
Kori Riddick <entense@att.net>
2022-10-10 14:55:29 -0400

    update saga image

--- components/svg/partner-logos/saga_logo-png.tsx
+++ components/svg/partner-logos/saga_logo-png.tsx
@@ -2,14 +2,12 @@ import CloudImage from "@components/cloud-image";
 
 const SagaLogo = () => {
   return (
-    <div className="bg-white/75 p-4">
-      <CloudImage
-        id="website-assets/saga_logo_zrh2t2"
-        alt="Saga Logo"
-        width={150}
-        height={150}
-      />
-    </div>
+    <CloudImage
+      id="website-assets/saga_logo_ifk2az"
+      alt="Saga Logo"
+      width={150}
+      height={150}
+    />
   );
 };
 export default SagaLogo;
