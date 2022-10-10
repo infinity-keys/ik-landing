ae788055f1e9613653c0febe15f957113ebd6b58
Kori Riddick <entense@att.net>
2022-10-10 13:55:22 -0400

    move partner images to cloudinary

--- components/svg/partner-logos/san_logo-png.tsx
+++ components/svg/partner-logos/san_logo-png.tsx
@@ -1,6 +1,13 @@
-import Image from "next/image";
-import san_logo from "../../svg/partner-logos/san_logo.png";
+import CloudImage from "@components/cloud-image";
+
 const SanLogo = () => {
-  return <Image src={san_logo} alt="Saga Logo" width="700px" height="700px" />;
+  return (
+    <CloudImage
+      id="website-assets/san_logo_dijhbm"
+      alt="Saga Logo"
+      width={150}
+      height={150}
+    />
+  );
 };
 export default SanLogo;
