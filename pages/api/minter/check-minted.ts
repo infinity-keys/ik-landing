62025129bfaf7613e5187bf9df2de7fb4ab64eb4
Hamilton Evans <38670367+ham-evans@users.noreply.github.com>
2022-11-01 14:02:36 -0700

    ham/api for nfts (#534)
    working

---
+++ pages/api/minter/check-minted.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { chainIds, contractLookup } from "@lib/walletConstants";
+import { castArray } from "lodash";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { tokenids } = req.query;
+
+  const tokenIds = castArray(tokenids);
+
+  const contractPromises = chainIds.map((chainId) =>
+    contractLookup[chainId].totalSupplyAll()
+  );
+
+  const contractClaims = await Promise.all(contractPromises);
+
+  const tokensMinted = tokenIds.map((token) => {
+    return contractClaims.flatMap((totalSupply) => {
+      const tokenId = parseInt(token, 10);
+      return totalSupply[tokenId].toNumber() > 0;
+    });
+  });
+
+  const returnArray = tokensMinted.map((bools) => bools.some((b) => b));
+
+  res.json({ tokensMinted: returnArray });
+}
