53bfae18dc3db2adee6fe9d51b00aa17f4f110e9
Christopher Bloom <bloomcb@gmail.com>
2022-11-15 23:44:17 -0800

    Puzzles successfully create steps in migration groups

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,30 +1,56 @@
 import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
+import type { RewardableType, StepType } from 'api/types/graphql'
+import { groupBy } from 'lodash'
 import fetch from 'node-fetch'
+import { z } from 'zod'
 
 // @TODO: use standalone creates on a loop to create user and ik org. Then loop
 // all rewardables as individual creates so the deep nesting creates can happen.
 
 // Original data
 const { GRAPHQL_ENDPOINT, HASURA_GRAPHQL_ADMIN_SECRET } = process.env
+
 // Get all original data
 const query = `query AllData {
   puzzles {
     simple_name
-    migration_puzzle
-    migration_step
     solution
     success_message
     fail_message
     instructions
     challenge
     list_publicly
+    migration_puzzle
+    migration_step
   }
 }`
 
+const ApiPuzzle = z.object({
+  simple_name: z.string(),
+  solution: z.string(),
+  success_message: z.nullable(z.string()),
+  fail_message: z.nullable(z.string()),
+  instructions: z.nullable(z.string()),
+  challenge: z.nullable(z.string()),
+  list_publicly: z.boolean(),
+  migration_puzzle: z.nullable(z.string()),
+  migration_step: z.nullable(z.string()),
+})
+
+const ApiResponse = z.object({
+  data: z.object({
+    puzzles: z.array(ApiPuzzle),
+  }),
+})
+
+// Stable IK org ID
+const ikCuid = 'cla9yay7y003k08la2z4j2xrv'
+
 export default async () => {
   try {
-    const v1IkData = await fetch(GRAPHQL_ENDPOINT, {
+    // Fetch all data we need to migrate
+    const apiRaw = await fetch(GRAPHQL_ENDPOINT, {
       method: 'POST',
       headers: {
         'x-hasura-admin-secret': HASURA_GRAPHQL_ADMIN_SECRET,
@@ -35,50 +61,6 @@ export default async () => {
         variables: {},
       }),
     }).then((res) => res.json())
-
-    const puzzles = v1IkData.data.puzzles.slice(0, 1)
-    // const puzzles = v1IkData.data.puzzles
-    console.log(JSON.stringify(puzzles, null, 2))
-
-    type RewardableType = 'PUZZLE'
-    type StepType = 'SIMPLE_TEXT'
-
-    const rewardables = {
-      create: puzzles.map((puzzle) => {
-        console.log(puzzle)
-        const rewardable = {
-          name: puzzle.simple_name,
-          slug: puzzle.simple_name,
-          type: 'PUZZLE' as RewardableType,
-          explanation: puzzle.instructions,
-          successMessage: puzzle.success_message, // just dupe what's in step for now
-          puzzle: {
-            create: {
-              steps: {
-                create: [
-                  {
-                    failMessage: puzzle.fail_message,
-                    challenge: puzzle.challenge,
-                    successMessage: puzzle.success_message,
-                    type: 'SIMPLE_TEXT' as StepType,
-                    stepSimpleText: {
-                      create: {
-                        solution: puzzle.solution,
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-          },
-        }
-
-        return rewardable
-      }),
-    }
-
-    console.log(JSON.stringify(rewardables, null, 2))
-
     //
     // Manually seed via `yarn rw prisma db seed`
     // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
@@ -89,20 +71,7 @@ export default async () => {
       "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
     )
 
-    // const orgs: Prisma.OrganizationCreateArgs['data'][] = [
-    //   { id: 'cla98s0cm000008mh66occsh4', name: 'Infinity Keys', slug: 'ik' },
-    // ]
-    // const createdOrgs = await Promise.all(
-    //   orgs.map(async (org) => {
-    //     const record = await db.organization.create({ data: org })
-    //     console.log(record)
-    //   })
-    // )
-    // console.log(createdOrgs)
-
-    const ikCuid = 'cla9yay7y003k08la2z4j2xrv'
-
-    const ikData: Prisma.UserCreateArgs['data'][] = [
+    const userOrg: Prisma.UserCreateArgs['data'][] = [
       {
         username: 'Herp',
         organizations: {
@@ -113,102 +82,111 @@ export default async () => {
                   id: ikCuid,
                   name: 'Infinity Keys',
                   slug: 'ik',
-                  // rewardables,
-                  // rewardables: {
-                  //   create: [
-                  //     {
-                  //       name: 'Not Right',
-                  //       slug: 'notright',
-                  //       type: 'PUZZLE',
-                  //       explanation: 'This is a starter puzzle',
-                  //       puzzle: {
-                  //         create: {
-                  //           steps: {
-                  //             create: [
-                  //               {
-                  //                 failMessage: 'You failed',
-                  //                 successMessage: 'You did it!',
-                  //                 challenge: 'Derp challenge',
-                  //                 type: 'SIMPLE_TEXT',
-                  //                 stepSimpleText: {
-                  //                   create: {
-                  //                     solution: 'wrong',
-                  //                   },
-                  //                 },
-                  //               },
-                  //             ],
-                  //           },
-                  //         },
-                  //       },
-                  //     },
-                  //   ],
-                  // },
                 },
               },
             },
           ],
         },
-        // organizations: {
-        //   create: [
-        //     {
-        //       orgId: ikCuid,
-        //       organization: {
-        //         create: {
-        //           // id: 'cla98s0cm000008mh66occsh4', // Keep this stable
-        //           id: ikCuid,
-        //           name: 'Infinity Keys',
-        //           slug: 'ik',
-        //           rewardables: {
-        //             create: [
-        //               {
-        //                 name: 'Not Right',
-        //                 slug: 'notright',
-        //                 type: 'PUZZLE',
-        //                 puzzle: {
-        //                   create: {
-        //                     steps: {
-        //                       create: [
-        //                         {
-        //                           failMessage: 'You failed',
-        //                           successMessage: 'You did it!',
-        //                           challenge: 'Derp challenge',
-        //                           type: 'SIMPLE_TEXT',
-        //                           stepSimpleText: {
-        //                             create: {
-        //                               solution: 'wrong',
-        //                             },
-        //                           },
-        //                         },
-        //                       ],
-        //                     },
-        //                   },
-        //                 },
-        //               },
-        //             ],
-        //           },
-        //         },
-        //       },
-        //     },
-        //   ],
-        // },
       },
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
     ]
 
-    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
     // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      ikData.map(async (data: Prisma.UserCreateArgs['data']) => {
+    await Promise.all(
+      userOrg.map(async (data: Prisma.UserCreateArgs['data']) => {
         const record = await db.user.create({ data })
+        console.log(record)
+      })
+    )
+
+    // Rewardables/Puzzles
+
+    // Validate and type incoming data
+    const puzzles = ApiResponse.parse(apiRaw).data.puzzles
+
+    // Group by the named puzzle flagged in migration column
+    const migratePuzzles: Prisma.RewardableCreateArgs['data'][] =
+      Object.entries(groupBy(puzzles, 'migration_puzzle')).reduce(
+        (acc, curr) => {
+          const [puzzleGroup, puzzles] = curr
+
+          if (puzzleGroup === 'null') {
+            const rewardables = puzzles.map((puzzle) => ({
+              name: puzzle.simple_name,
+              slug: puzzle.simple_name,
+              type: 'PUZZLE' as RewardableType,
+              explanation: puzzle.instructions || '',
+              successMessage: puzzle.success_message, // just dupe what's in step for now
+              organization: {
+                connect: {
+                  id: ikCuid,
+                },
+              },
+              puzzle: {
+                create: {
+                  steps: {
+                    create: [
+                      {
+                        failMessage: puzzle.fail_message,
+                        challenge: puzzle.challenge,
+                        successMessage: puzzle.success_message,
+                        type: 'SIMPLE_TEXT' as StepType,
+                        stepSimpleText: {
+                          create: {
+                            solution: puzzle.solution,
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+              },
+            }))
+            acc = [...acc, ...rewardables]
+            return acc
+          }
+
+          // These are puzzles that are being combined into mult-step in new system
+          const rewardable = {
+            name: puzzleGroup,
+            slug: puzzleGroup,
+            type: 'PUZZLE' as RewardableType,
+            explanation: puzzles[0].instructions,
+            successMessage: puzzles[puzzles.length - 1].success_message, // just dupe what's in step for now
+            organization: {
+              connect: {
+                id: ikCuid,
+              },
+            },
+            puzzle: {
+              create: {
+                steps: {
+                  create: puzzles.map((puzzle) => ({
+                    failMessage: puzzle.fail_message,
+                    challenge: puzzle.challenge,
+                    successMessage: puzzle.success_message,
+                    type: 'SIMPLE_TEXT' as StepType,
+                    stepSortWeight: parseInt(puzzle.migration_step, 10),
+                    stepSimpleText: {
+                      create: {
+                        solution: puzzle.solution,
+                      },
+                    },
+                  })),
+                },
+              },
+            },
+          }
+          acc = [...acc, rewardable]
+          return acc
+        },
+        [] as Prisma.RewardableCreateArgs['data'][]
+      )
+
+    // console.log(migratePuzzles)
+
+    await Promise.all(
+      migratePuzzles.map(async (data) => {
+        const record = await db.rewardable.create({ data })
         console.log(record)
       })
     )
