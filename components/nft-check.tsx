68d3194f58e8eab458d9c2f93f5910b5557cf8cf
chair28980 <vrycmfy@protonmail.com>
2022-11-22 12:13:22 -0500

    update typo

--- components/nft-check.tsx
+++ components/nft-check.tsx
@@ -66,7 +66,7 @@ const NftCheck = ({
         <Heading as="h2" visual="s">
           {nftApproval
             ? "Challenge complete!"
-            : "This chellenge requires a digital key. Please connect your web3 wallet to unlock."}
+            : "This challenge requires a digital key. Please connect your web3 wallet to unlock."}
         </Heading>
       </div>
 
