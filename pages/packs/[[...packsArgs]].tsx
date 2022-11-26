8db5a088bf18f697c8fddb7c8408c1da2c5c8833
Tawnee <allprt89@gmail.com>
2022-11-25 17:03:40 -0700

    remove test

--- pages/packs/[[...packsArgs]].tsx
+++ pages/packs/[[...packsArgs]].tsx
@@ -81,7 +81,6 @@ export async function getStaticProps({
   });
 
   const numberOfPacks = count.aggregate?.count || 0;
-  console.log("numberOfPacks: ", numberOfPacks);
   const numOfPages = Math.ceil(numberOfPacks / limit);
 
   return {
