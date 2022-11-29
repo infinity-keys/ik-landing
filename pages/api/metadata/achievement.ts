3cb48d2d98bbbdd75f3914bab58a09222219877b
Christopher Bloom <bloomcb@gmail.com>
2022-11-29 08:22:47 -0800

    Metadata check no longer hits chain

--- pages/api/metadata/achievement.ts
+++ pages/api/metadata/achievement.ts
@@ -4,6 +4,8 @@ import { IKAchievementABI__factory } from "@lib/generated/ethers-contract";
 import { CONTRACT_ADDRESS_POLYGON, POLYGON_RPC } from "@lib/walletConstants";
 import { ethers } from "ethers";
 
+const ethersJsonRpcProvider = new ethers.providers.JsonRpcProvider(POLYGON_RPC);
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -12,20 +14,15 @@ export default async function handler(
 
   if (typeof tokenid !== "string") return res.status(500).end();
 
-  const nftId = parseInt(tokenid, 10);
-
-  const contractPolygon = IKAchievementABI__factory.connect(
-    CONTRACT_ADDRESS_POLYGON,
-    new ethers.providers.JsonRpcProvider(POLYGON_RPC)
-  );
-  const numTokens = (await contractPolygon.totalSupplyAll()).length;
-  if (nftId >= numTokens) return res.status(500).end();
-
+  // Does our contract exist for this tokenid?
   const gql = await gqlApiSdk();
   const { nft_metadata_by_pk } = await gql.NftMetadata({
-    nftId,
+    nftId: parseInt(tokenid, 10),
     contractName: "achievement",
   });
 
+  if (!nft_metadata_by_pk?.data) return res.status(500).end();
+
+  // Finally, return
   res.json(nft_metadata_by_pk?.data);
 }
