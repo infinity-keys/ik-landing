bb1b7633b3283e5ff567c53dfdbd626d43d75189
Christopher Bloom <bloomcb@gmail.com>
2022-11-29 09:07:39 -0800

    Validate JWT, check for existence of nft token in db before hitting chain

--- pages/api/minter/check-claimed.ts
+++ pages/api/minter/check-claimed.ts
@@ -2,6 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { chainIds } from "@lib/walletConstants";
 import { contractLookup } from "@lib/contractLookup";
+import { gqlApiSdk } from "@lib/server";
+import { IK_ID_COOKIE } from "@lib/constants";
+import { verifyToken } from "@lib/jwt";
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,6 +14,34 @@ export default async function handler(
 
   if (typeof tokenId !== "string" || typeof account !== "string")
     return res.status(500).end();
+
+  const tokenIdAsInt = parseInt(tokenId, 10);
+
+  const jwtToken = req.cookies[IK_ID_COOKIE];
+  if (!jwtToken) return res.status(401).end();
+
+  // Validate token first, no valid JWT, bail
+  try {
+    await verifyToken(jwtToken);
+  } catch (e) {
+    // Bad token
+    return res.status(401).end();
+  }
+
+  // Next gate: check the DB for tokenId in both NFT table AND! pack table
+  const gql = await gqlApiSdk();
+  const results = await gql.CheckNftOrPackForToken({
+    tokenIdInt: tokenIdAsInt,
+    tokenIdNum: tokenIdAsInt,
+  });
+
+  const existsNfts = results.nfts.some((nft) => !!nft.tokenId);
+  const existsPacks = results.packs.some((pack) => !!pack.nftId);
+
+  // The nft token id sent in does not exist in our
+  if (!existsNfts || !existsPacks) {
+    return res.status(404).end();
+  }
 
   try {
     const contractPromises = chainIds.map((chainId) =>
