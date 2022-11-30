da22b150e8874478900eeb205d8c489af0c91002
chair <29414216+chair28980@users.noreply.github.com>
2022-11-30 10:12:56 -0500

    rotate infura keys (#577)

--- lib/contractLookup.ts
+++ lib/contractLookup.ts
@@ -12,9 +12,9 @@ import {
 } from "./walletConstants";
 
 export const AVAX_RPC = "https://api.avax.network/ext/bc/C/rpc";
-export const ETH_RPC = `https://mainnet.infura.io/v3/9d0cdbccaa254f32812c75779eed3b32`;
+export const ETH_RPC = `https://mainnet.infura.io/v3/05e9523d724744c0b1f398a7ebcb602e`;
 export const OPTIMISM_RPC =
-  "https://optimism-mainnet.infura.io/v3/9d0cdbccaa254f32812c75779eed3b32";
+  "https://optimism-mainnet.infura.io/v3/05e9523d724744c0b1f398a7ebcb602e";
 export const POLYGON_RPC = "https://polygon-rpc.com";
 
 export const chainRPCLookup: {
