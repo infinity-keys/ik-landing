7e86693cf4e6a99f8647e260fe5a795800f145f1
Christopher Bloom <bloomcb@gmail.com>
2022-11-30 10:04:36 -0800

    Add headers with random seed for infura safety (#580)

--- lib/contractLookup.ts
+++ lib/contractLookup.ts
@@ -12,7 +12,8 @@ import {
 } from "./walletConstants";
 
 export const AVAX_RPC = "https://api.avax.network/ext/bc/C/rpc";
-export const ETH_RPC = "https://mainnet.infura.io/v3/65a0c8457d9045fcb68d976e951a1d82";
+export const ETH_RPC =
+  "https://mainnet.infura.io/v3/65a0c8457d9045fcb68d976e951a1d82";
 export const OPTIMISM_RPC =
   "https://optimism-mainnet.infura.io/v3/65a0c8457d9045fcb68d976e951a1d82";
 export const POLYGON_RPC = "https://polygon-rpc.com";
@@ -26,14 +27,22 @@ export const chainRPCLookup: {
   [OPTIMISM_CHAIN_ID]: OPTIMISM_RPC,
 };
 
-const ethRpcJsonRpcProvider = new ethers.providers.JsonRpcProvider(ETH_RPC);
+const ethRpcJsonRpcProvider = new ethers.providers.JsonRpcProvider({
+  url: ETH_RPC,
+  headers: {
+    Origin: "clb3yaxak0001356n8iawecm4.infinitykeys.io",
+  },
+});
+const optimismRpcJsonRpcProvider = new ethers.providers.JsonRpcProvider({
+  url: OPTIMISM_RPC,
+  headers: {
+    Origin: "clb3yaxak0001356n8iawecm4.infinitykeys.io",
+  },
+});
 const polygonRpcJsonRpcProvider = new ethers.providers.JsonRpcProvider(
   POLYGON_RPC
 );
 const avaxRpcJsonRpcProvider = new ethers.providers.JsonRpcProvider(AVAX_RPC);
-const optimismRpcJsonRpcProvider = new ethers.providers.JsonRpcProvider(
-  OPTIMISM_RPC
-);
 
 export const contractLookup: {
   [key: number]: ReturnType<typeof IKAchievementABI__factory.connect>;
