698f810fc65d31f9138c86cafbc0378dac30de0e
Tawnee <allprt89@gmail.com>
2022-12-07 13:15:15 -0700

    add save to db example

--- web/src/pages/AuthPage/AuthPage.tsx
+++ web/src/pages/AuthPage/AuthPage.tsx
@@ -3,16 +3,26 @@ import { useRef, useState } from 'react'
 import { z } from 'zod'
 
 import { useAuth } from '@redwoodjs/auth'
+import { useMutation } from '@redwoodjs/web'
 
 import Button from 'src/components/Button'
 import Wrapper from 'src/components/Wrapper/Wrapper'
 
+const MUTATION = gql`
+  mutation CreateNewUser($email: String!) {
+    createUser(input: { nonce: "example", siteRole: VERIFIED, email: $email }) {
+      id
+    }
+  }
+`
 const isValidEmail = (email) => z.string().email().safeParse(email).success
 
 const AuthPage = () => {
   const { logIn, logOut, isAuthenticated } = useAuth()
   const [errorMessage, setErrorMessage] = useState('')
   const emailRef = useRef(null)
+
+  const [create] = useMutation(MUTATION)
 
   const handleClick = async () => {
     if (isAuthenticated) return logOut()
@@ -26,6 +36,7 @@ const AuthPage = () => {
 
     try {
       const user = await logIn({ email })
+      create({ variables: { email } })
       console.log('user: ', user)
     } catch (e) {
       setErrorMessage('Problem sending email')
@@ -37,8 +48,8 @@ const AuthPage = () => {
       <div className="relative">
         {!isAuthenticated && (
           <input
-            type="text"
-            placeholder="email"
+            type="email"
+            placeholder="Your Email"
             ref={emailRef}
             className="mr-4 rounded border border-turquoise bg-transparent text-lg text-white placeholder:text-gray-150"
           />
