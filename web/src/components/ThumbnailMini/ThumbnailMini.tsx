1e69db07d379e5ad15ebbaf57f6d42993ce9e5f0
Tawnee <allprt89@gmail.com>
2022-12-28 13:06:26 -0700

    Eng-547: add step UI (#558)
    * add react share package set up
    
    * update styles
    
    * get build working
    
    * add readme, make lensbutton default export
    
    * update readme
    
    * shrink image
    
    * update readme
    
    * revert homepage changes
    
    * remove removeStyles
    
    * remove default import
    
    * change umd to cjs
    
    * update readme
    
    * update readme, add npmignore
    
    * update json paths
    
    * fix spelling error
    
    * add utils, constants, and types packages
    
    * remove vite.timestamp
    
    * move utils into one core package
    
    * fix lensshare package.json
    
    * add gitigores and remove tsbuildinfo
    
    * use vite to build packages
    
    * add packages to api
    
    * update import, remove duplicate libs
    
    * fix imports
    
    * add step page, thumbnail mini, new step progress
    
    * hide steps if there's only one
    
    * update border colors and flex
    
    * use rem
    
    * add dynamic href to thumbnail
    
    * add checkmarks
    
    * update step layout
    
    * add puzzle header
    
    * add progress util fn. update thumbnail type.

---
+++ web/src/components/ThumbnailMini/ThumbnailMini.tsx
@@ -0,0 +1,54 @@
+import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon'
+import LockOpenIcon from '@heroicons/react/24/solid/LockOpenIcon'
+import { ThumbnailProgress } from '@infinity-keys/core'
+import Avatar from 'boring-avatars'
+import clsx from 'clsx'
+
+import { Link } from '@redwoodjs/router'
+
+interface ThumbnailMiniProps {
+  name: string
+  step: number
+  progress: ThumbnailProgress
+  href?: string
+}
+
+const ThumbnailMini = ({ name, step, progress, href }: ThumbnailMiniProps) => {
+  return (
+    <Link
+      className={clsx(
+        'relative flex w-full max-w-[10rem] items-center rounded-lg border bg-blue-800 py-3 px-3 text-center shadow lg:px-4',
+        progress === ThumbnailProgress.Current
+          ? 'border-turquoise'
+          : 'border-transparent',
+        {
+          'cursor-pointer transition hover:border-turquoise':
+            progress === ThumbnailProgress.Completed,
+          'opacity-60 grayscale':
+            progress === ThumbnailProgress.NotCompleted ||
+            progress === ThumbnailProgress.Unknown,
+        }
+      )}
+      to={progress === ThumbnailProgress.Completed && href ? href : null}
+    >
+      <span className="absolute top-1 right-1 flex h-4 w-4 items-center justify-center rounded-full ">
+        {progress === ThumbnailProgress.Completed ? (
+          <LockOpenIcon className="h-3 w-3 text-turquoise" />
+        ) : (
+          <LockClosedIcon className="h-3 w-3 text-turquoise" />
+        )}
+      </span>
+
+      <Avatar
+        size={28}
+        name={name + step}
+        variant="marble"
+        colors={['#101D42', '#E400FF', '#3FCCBB', '#8500AC', '#303B5B']}
+      />
+
+      <p className="ml-2 text-sm text-gray-150 lg:ml-4">Step {step}</p>
+    </Link>
+  )
+}
+
+export default ThumbnailMini
