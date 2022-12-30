05b30d1753463220c3d91e84e62752e2464d16ec
IvySwing <entense@att.net>
2022-12-30 21:29:07 +0000

    code cleanup

--- api/src/functions/discord-bot.ts
+++ api/src/functions/discord-bot.ts
@@ -6,8 +6,6 @@ const Moralis = require('moralis').default
 
 //const web3 = new Web3()
 require('dotenv').config()
-const app = express()
-const port = 3000
 
 const client = new discord.Client({
   intents: [],
@@ -15,43 +13,36 @@ const client = new discord.Client({
 
 client.login(process.env.PASS)
 
-app.use(express.json())
-
-app.post('/webhook', async (req, res) => {
-  const { body, headers } = req
+export const handler = async (event) => {
+  const { body, headers } = event
 
   try {
-    Moralis.Streams.verifySignature({
+    await Moralis.Streams.verifySignature({
       body,
       signature: headers['x-signature'],
     })
 
-    // console.log(headers);
-    // console.log(body);
+    // console.log(body)
 
     const from = body.txs[0].fromAddress
-    //console.log(from);
     const tokenId = parseInt(body.logs[0].topic1, 16)
-    //console.log(tokenId);
     const chainId = parseInt(body.chainId, 16)
-    //console.log(chainId);
     const chain = chainIdLookup[chainId]
 
     const channel = await client.channels.fetch(process.env.CHANNEL)
     channel.send(`Token ID ${tokenId} claimed by ${from} from ${chain}`)
 
-    return res.status(200).json()
+    return {
+      statusCode: 200,
+    }
   } catch (e) {
     console.log(e)
     console.log('Not Moralis')
-    return res.status(400).json()
+    return {
+      statusCode: 400,
+    }
   }
-})
-
+}
 Moralis.start({
   apiKey: process.env.APIKEY,
-}).then(() => {
-  app.listen(port, () => {
-    console.log(`Listening to my stream`)
-  })
 })
