a03ff1377eaed12b2ee0fe15ce533d2f8eff514a
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2023-01-03 11:45:22 -0500

    Itsmekori/eng 625 create discord mint tracking bot (#622)
    * add discord bot and .env file w/associated pkgs
    
    * code cleanup
    
    * update syntax to manually parse JSON body and code cleanup

---
+++ api/src/functions/discord-bot.ts
@@ -0,0 +1,45 @@
+import { chainIdLookup } from '@infinity-keys/constants'
+
+const discord = require('discord.js')
+const Moralis = require('moralis').default
+
+const client = new discord.Client({
+  intents: [],
+})
+
+client.login(process.env.PASS)
+
+export const handler = async (event) => {
+  const { body, headers } = event
+  const parsedBody = await JSON.parse(body)
+
+  try {
+    await Moralis.Streams.verifySignature({
+      body: parsedBody,
+      signature: headers['x-signature'],
+    })
+
+    //console.log(body)
+
+    const from = parsedBody.txs[0].fromAddress
+    const tokenId = parseInt(parsedBody.logs[0].topic1, 16)
+    const chainId = parseInt(parsedBody.chainId, 16)
+    const chain = chainIdLookup[chainId]
+
+    const channel = await client.channels.fetch(process.env.CHANNEL)
+    channel.send(`Token ID ${tokenId} claimed by ${from} from ${chain}`)
+
+    return {
+      statusCode: 200,
+    }
+  } catch (e) {
+    console.log(e)
+    console.log('Not Moralis')
+    return {
+      statusCode: 400,
+    }
+  }
+}
+Moralis.start({
+  apiKey: process.env.APIKEY,
+})
