432f951aebfd35efa0b21755eec97249822fe8bd
chair28980 <vrycmfy@protonmail.com>
2023-01-03 14:04:54 -0500

    update env variable names

--- api/src/functions/discord-bot.ts
+++ api/src/functions/discord-bot.ts
@@ -7,11 +7,14 @@ const client = new discord.Client({
   intents: [],
 })
 
-client.login(process.env.PASS)
+client.login(process.env.DISCORD_MINT_BOT_KEY)
+
+Moralis.start({
+  apiKey: process.env.MORALIS_API_KEY,
+})
 
 export const handler = async (event) => {
   const { body, headers } = event
-  console.log({ body, headers })
   const parsedBody = await JSON.parse(body)
 
   try {
@@ -20,19 +23,16 @@ export const handler = async (event) => {
       signature: headers['x-signature'],
     })
 
-
     if (parsedBody.txs.length === 0) {
       return { statusCode: 200 }
     }
-
-
 
     const from = parsedBody.txs[0].fromAddress
     const tokenId = parseInt(parsedBody.logs[0].topic1, 16)
     const chainId = parseInt(parsedBody.chainId, 16)
     const chain = chainIdLookup[chainId]
 
-    const channel = await client.channels.fetch(process.env.CHANNEL)
+    const channel = await client.channels.fetch(process.env.MINT_CHANNEL)
     channel.send(`Token ID ${tokenId} claimed by ${from} from ${chain}`)
 
     return {
@@ -46,6 +46,3 @@ export const handler = async (event) => {
     }
   }
 }
-Moralis.start({
-  apiKey: process.env.APIKEY,
-})
