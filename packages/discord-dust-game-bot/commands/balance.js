8347f98463d8c7964e9dac92ba70064b76aff302
IvySwing <entense@att.net>
2023-02-20 20:03:37 +0000

    added deferReply to commands

--- packages/discord-dust-game-bot/commands/balance.js
+++ packages/discord-dust-game-bot/commands/balance.js
@@ -14,6 +14,7 @@ module.exports = {
 		const embedBalance = new EmbedBuilder()
 			.setDescription(`I’m sure you’ve been working hard. Let me collect those files… According to the records you’ve located \`${balance}\` pages. Is that all? Well then.`)
 			.setColor('c3b4f7');
-		await interaction.reply({ embeds: [embedBalance] });
+      await interaction.deferReply();
+    await interaction.editReply({ embeds: [embedBalance] });
 	},
 };
--- packages/discord-dust-game-bot/commands/daily.js
+++ packages/discord-dust-game-bot/commands/daily.js
@@ -15,13 +15,15 @@ module.exports = {
 			const embedNoDaily = new EmbedBuilder()
 				.setDescription('Oh dear, all that time...for nothing. Well, there\'s always tomorrow.  In the meantime, why don’t you take a cup of tea...elsewhere.')
 				.setColor('c3b4f7');
-			await interaction.reply({ embeds: [embedNoDaily] });
+        await interaction.deferReply();
+			await interaction.editReply({ embeds: [embedNoDaily] });
 		}
 
 		const embedDaily = new EmbedBuilder()
 			.setThumbnail('https://res.cloudinary.com/infinity-keys/image/upload/v1676060677/discord-bot/magic_far_shot_angle_anime_style_black_person_older_person_occu_e90c7a75-60da-4076-a367-992a0a87f3f1_t0djmd.png')
 			.setDescription(`Ah, one of my two favorite times of the day—when you return from a deep search of the library and bring back many pages.  Today you collected \`${daily.reward} pages 📜\`, splendid!  The other is of course, tea time. Cheers...`)
 			.setColor('c3b4f7');
-		await interaction.reply({ embeds: [embedDaily] });
+      await interaction.deferReply();
+		await interaction.eitReply({ embeds: [embedDaily] });
 	},
 };
