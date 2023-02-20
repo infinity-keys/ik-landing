5c9a46c5b3b86fbcfb01d8085668ee1813c1043e
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2023-02-20 16:57:41 -0500

    Possible fix discord bot timeout (#705)
    * added deferReply to commands
    
    * update code fix timeout error
    
    * update fix to code
    
    * possible fix
    
    ---------
    
    Co-authored-by: Tawnee <allprt89@gmail.com>

--- packages/discord-dust-game-bot/commands/balance.js
+++ packages/discord-dust-game-bot/commands/balance.js
@@ -1,20 +1,25 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder } = require('discord.js');
-const eco = require('../ecoDB');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { EmbedBuilder } = require('discord.js')
+
+const eco = require('../ecoDB')
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('balance')
-		.setDescription('Gracefully inquire whether Archivist Nebo has the records of your page collection efforts at hand.'),
-	async execute(interaction) {
-    await interaction.deferReply();
-    const { guild, member } = interaction;
-		const balance = eco.balance.get(member.id, guild.id);
-		// const username = interaction.user.username;
+  data: new SlashCommandBuilder()
+    .setName('balance')
+    .setDescription(
+      'Gracefully inquire whether Archivist Nebo has the records of your page collection efforts at hand.'
+    ),
+  async execute(interaction) {
+		// await interaction.deferReply();
+		const { guild, member } = interaction
+    const balance = eco.balance.get(member.id, guild.id)
+    // const username = interaction.user.username;
 
-		const embedBalance = new EmbedBuilder()
-    .setDescription(`I’m sure you’ve been working hard. Let me collect those files… According to the records you’ve located \`${balance}\` pages. Is that all? Well then.`)
-    .setColor('c3b4f7');
-    await interaction.editReply({ embeds: [embedBalance] });
-	},
-};
+    const embedBalance = new EmbedBuilder()
+			.setDescription(
+        `I’m sure you’ve been working hard. Let me collect those files… According to the records you’ve located \`${balance}\` pages. Is that all? Well then.`
+			)
+			.setColor('c3b4f7');
+		await interaction.editReply({ embeds: [embedBalance] })
+  },
+}
