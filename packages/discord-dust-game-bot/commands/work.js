46bdc71e320085db63e31a877ffb77e206eac21c
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2023-02-21 17:46:09 -0500

    update commands (#711)
    update commands to remove deferReply

--- packages/discord-dust-game-bot/commands/work.js
+++ packages/discord-dust-game-bot/commands/work.js
@@ -15,12 +15,12 @@ module.exports = {
 			const embedNoWork = new EmbedBuilder()
 				.setDescription('Unfortunately, your search has not produced...ahem...results... Come back at the appropriate time to try the stacks again, perhaps you’ll feel a bit more refreshed.')
 				.setColor('c3b4f7');
-			await interaction.editReply({ embeds: [embedNoWork] });
+			await interaction.reply({ embeds: [embedNoWork] });
 		}
 
 		const embedWork = new EmbedBuilder()
 			.setDescription(`It appears that your search has not been in vain. I thank you my friend. You have collected \`${work.reward} pages 📜\`. Come back in another hour to seek within the stacks once more.`)
 			.setColor('c3b4f7');
-		await interaction.editReply({ embeds: [embedWork] });
+		await interaction.reply({ embeds: [embedWork] });
 	},
 };
