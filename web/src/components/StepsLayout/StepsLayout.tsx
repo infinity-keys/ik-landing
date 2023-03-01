ef8e0ea7a1cd1183d75356858a6236557f369dbe
Tawnee <allprt89@gmail.com>
2023-03-01 10:45:55 -0700

    lazy load step type components

--- web/src/components/StepsLayout/StepsLayout.tsx
+++ web/src/components/StepsLayout/StepsLayout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, lazy, Suspense } from 'react'
 
 import { getThumbnailProgress } from '@infinity-keys/core'
 import { FindAnonStepQuery, FindStepQuery } from 'types/graphql'
@@ -7,8 +7,7 @@ import { useAuth } from '@redwoodjs/auth'
 import { routes, useParams } from '@redwoodjs/router'
 
 import CollapsibleMarkdown from 'src/components/CollapsibleMarkdown/CollapsibleMarkdown'
-import NftCheckButton from 'src/components/NftCheckButton/NftCheckButton'
-import SimpleTextInput from 'src/components/SimpleTextInput/SimpleTextInput'
+import LoadingIcon from 'src/components/LoadingIcon/LoadingIcon'
 import ThumbnailMini from 'src/components/ThumbnailMini/ThumbnailMini'
 
 interface StepsLayoutProps extends PropsWithChildren {
@@ -16,6 +15,13 @@ interface StepsLayoutProps extends PropsWithChildren {
   puzzle: FindAnonStepQuery['puzzle'] | FindStepQuery['puzzle']
   step: FindAnonStepQuery['step'] | FindStepQuery['step']
 }
+
+const SimpleTextInput = lazy(
+  () => import('src/components/SimpleTextInput/SimpleTextInput')
+)
+const NftCheckButton = lazy(
+  () => import('src/components/NftCheckButton/NftCheckButton')
+)
 
 const StepsLayout = ({
   currentStepIndex,
@@ -28,31 +34,33 @@ const StepsLayout = ({
 
   return (
     <div>
-      {step && (
-        <div>
-          {step.type === 'SIMPLE_TEXT' && (
-            <SimpleTextInput
-              count={step.stepSimpleText.solutionCharCount}
-              step={step}
-              puzzleId={puzzle.id}
-            />
-          )}
-
-          {step.type === 'NFT_CHECK' && (
-            <NftCheckButton step={step} puzzleId={puzzle.id} />
-          )}
-
-          <div className="mx-auto mt-12 mb-12 max-w-prose p-4 md:mt-16 md:mb-20">
-            {step.challenge && (
-              <CollapsibleMarkdown
-                title="Challenge"
-                content={step.challenge}
-                defaultOpen
+      <Suspense fallback={<LoadingIcon />}>
+        {step && (
+          <div>
+            {step.type === 'SIMPLE_TEXT' && (
+              <SimpleTextInput
+                count={step.stepSimpleText.solutionCharCount}
+                step={step}
+                puzzleId={puzzle.id}
               />
             )}
+
+            {step.type === 'NFT_CHECK' && (
+              <NftCheckButton step={step} puzzleId={puzzle.id} />
+            )}
+
+            <div className="mx-auto mt-12 mb-12 max-w-prose p-4 md:mt-16 md:mb-20">
+              {step.challenge && (
+                <CollapsibleMarkdown
+                  title="Challenge"
+                  content={step.challenge}
+                  defaultOpen
+                />
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </Suspense>
 
       {children}
 
