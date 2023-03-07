cb2259ab595a4fe5784b304c38c6a561fa4d9234
Tawnee <allprt89@gmail.com>
2023-03-07 10:28:10 -0700

    comment out component

--- web/src/components/EmailSubmission/EmailSubmission.tsx
+++ web/src/components/EmailSubmission/EmailSubmission.tsx
@@ -1,90 +1,90 @@
-import {
-  CreateSubmissionWithOptionalEmailMutation,
-  CreateSubmissionWithOptionalEmailMutationVariables,
-} from 'types/graphql'
+// import {
+//   CreateSubmissionWithOptionalEmailMutation,
+//   CreateSubmissionWithOptionalEmailMutationVariables,
+// } from 'types/graphql'
 
-import {
-  FieldError,
-  Form,
-  FormError,
-  Label,
-  TextField,
-  Submit,
-  SubmitHandler,
-} from '@redwoodjs/forms'
-import { useMutation } from '@redwoodjs/web'
-import { toast, Toaster } from '@redwoodjs/web/toast'
+// import {
+//   FieldError,
+//   Form,
+//   FormError,
+//   Label,
+//   TextField,
+//   Submit,
+//   SubmitHandler,
+// } from '@redwoodjs/forms'
+// import { useMutation } from '@redwoodjs/web'
+// import { toast, Toaster } from '@redwoodjs/web/toast'
 
-interface EmailSubmissionProps {
-  puzzleId: string
-  userId: string
-}
+// interface EmailSubmissionProps {
+//   puzzleId: string
+//   userId: string
+// }
 
-interface FormValues {
-  email: string
-}
+// interface FormValues {
+//   email: string
+// }
 
-const CREATE_SUBMISSION_MUTATION = gql`
-  mutation CreateSubmissionWithOptionalEmailMutation(
-    $input: CreateSubmissionInput!
-  ) {
-    createSubmissionWithOptionalEmail(input: $input) {
-      data
-      puzzleId
-    }
-  }
-`
+// const CREATE_SUBMISSION_MUTATION = gql`
+//   mutation CreateSubmissionWithOptionalEmailMutation(
+//     $input: CreateSubmissionInput!
+//   ) {
+//     createSubmissionWithOptionalEmail(input: $input) {
+//       data
+//       puzzleId
+//     }
+//   }
+// `
 
-const EmailSubmission = ({ puzzleId, userId }: EmailSubmissionProps) => {
-  const [create, { loading, error }] = useMutation<
-    CreateSubmissionWithOptionalEmailMutation,
-    CreateSubmissionWithOptionalEmailMutationVariables
-  >(CREATE_SUBMISSION_MUTATION, {
-    onCompleted: () => {
-      // @TODO: clear field
-      toast.success('Thank you for your submission!')
-    },
-  })
+// const EmailSubmission = ({ puzzleId, userId }: EmailSubmissionProps) => {
+//   const [create, { loading, error }] = useMutation<
+//     CreateSubmissionWithOptionalEmailMutation,
+//     CreateSubmissionWithOptionalEmailMutationVariables
+//   >(CREATE_SUBMISSION_MUTATION, {
+//     onCompleted: () => {
+//       // @TODO: clear field
+//       toast.success('Thank you for your submission!')
+//     },
+//   })
 
-  const onSubmit: SubmitHandler<FormValues> = ({ email }) => {
-    create({
-      variables: {
-        input: {
-          data: { email },
-          puzzleId,
-          userId,
-        },
-      },
-    })
-  }
+//   const onSubmit: SubmitHandler<FormValues> = ({ email }) => {
+//     create({
+//       variables: {
+//         input: {
+//           data: { email },
+//           puzzleId,
+//           userId,
+//         },
+//       },
+//     })
+//   }
 
-  return (
-    <div>
-      {/* @TODO: Toaster is hidden behind header (z-index) */}
-      <Toaster />
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
-        <FormError error={error} wrapperClassName="form-error" />
+//   return (
+//     <div>
+//       {/* @TODO: Toaster is hidden behind header (z-index) */}
+//       <Toaster />
+//       <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+//         <FormError error={error} wrapperClassName="form-error" />
 
-        <Label name="email" errorClassName="error">
-          Email
-        </Label>
-        <TextField
-          name="email"
-          validation={{
-            required: true,
-            pattern: {
-              value: /^[^@]+@[^.]+\..+$/,
-              message: 'Please enter a valid email address',
-            },
-          }}
-          errorClassName="error"
-        />
-        <FieldError name="email" className="error" />
+//         <Label name="email" errorClassName="error">
+//           Email
+//         </Label>
+//         <TextField
+//           name="email"
+//           validation={{
+//             required: true,
+//             pattern: {
+//               value: /^[^@]+@[^.]+\..+$/,
+//               message: 'Please enter a valid email address',
+//             },
+//           }}
+//           errorClassName="error"
+//         />
+//         <FieldError name="email" className="error" />
 
-        <Submit disabled={loading}>Save</Submit>
-      </Form>
-    </div>
-  )
-}
+//         <Submit disabled={loading}>Save</Submit>
+//       </Form>
+//     </div>
+//   )
+// }
 
-export default EmailSubmission
+// export default EmailSubmission
