ca49e25f9d8cd6d4aa9cd60726b3978d25c95a82
Tawnee <allprt89@gmail.com>
2023-03-08 09:47:40 -0700

    ENG-755: remove unsued steps sdl definitions (#741)
    remove unused steps sdl definitions

--- api/src/graphql/ik/steps.sdl.ts
+++ api/src/graphql/ik/steps.sdl.ts
@@ -1,17 +1,5 @@
 export const schema = gql`
-  type StepProgress {
-    stepId: String!
-    solved: Boolean!
-    stepSortWeight: Int!
-  }
-
-  type StepsByPuzzleIdResponse {
-    puzzle: Puzzle!
-    stepProgress: [StepProgress!]!
-  }
-
   type Query {
-    getStepsByPuzzleId(id: String!): StepsByPuzzleIdResponse @skipAuth
     optionalStep(id: String, puzzleId: String, stepNum: Int): Step @requireAuth
     anonOptionalStep(id: String, puzzleId: String, stepNum: Int): Step @skipAuth
   }
