55320696f360c79ff678eb35d484c28cb0123acd
chair28980 <vrycmfy@protonmail.com>
2023-03-11 14:50:55 -0500

    update icon image

--- api/src/functions/discord-vote-bot.ts
+++ api/src/functions/discord-vote-bot.ts
@@ -39,7 +39,7 @@ export const handler = async (event) => {
       .setAuthor({
         name: 'Infinity Keys',
         iconURL:
-          'https://res.cloudinary.com/infinity-keys/image/upload/v1671162913/ik-alpha-trophies/Ikey-Antique-Logo_dithbc.png',
+          'https://res.cloudinary.com/infinity-keys/image/upload/t_ik-nft-meta/website-assets/hold-your-gotchi-embed_a2y1pq.jpg',
         url: 'https://infinitykeys.io',
       })
       .setDescription('New Curve Vote Has Been Cast! 👻')
@@ -50,9 +50,9 @@ export const handler = async (event) => {
 
       .setTimestamp()
       .setFooter({
-        text: 'Vote Casted',
+        text: 'Voted',
         iconURL:
-          'https://res.cloudinary.com/infinity-keys/image/upload/v1671162913/ik-alpha-trophies/Ikey-Antique-Logo_dithbc.png',
+          'https://res.cloudinary.com/infinity-keys/image/upload/t_ik-nft-meta/website-assets/hold-your-gotchi-embed_a2y1pq.jpg',
       })
 
     const channel = await client.channels.fetch(process.env.VOTE_ALERT_CHANNEL)
