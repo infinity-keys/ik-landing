deea470098d530fde78a63e9dcdc8550989df0b7
Tawnee <allprt89@gmail.com>
2023-03-13 11:06:32 -0600

    ENG-769: userRewards in reconciliation for packs and puzzles (#755)
    * move reconcile into login function
    
    * use anon user id constant
    
    * work on reconciliation puzzle rewards
    
    * get puzzle rewards working
    
    * update reconciliation for pack and puzzle userrewards
    
    * remove new query from attempt
    
    * add comments
    
    * reset seed script
    
    * use createMany for userRewards, update profile styles

--- web/src/pages/ProfilePage/ProfilePage.tsx
+++ web/src/pages/ProfilePage/ProfilePage.tsx
@@ -7,6 +7,7 @@ import { useAuth } from '@redwoodjs/auth'
 import Button from 'src/components/Button'
 import LoadingIcon from 'src/components/LoadingIcon/LoadingIcon'
 import ProfileCell from 'src/components/ProfileCell'
+import useReconcileProgress from 'src/hooks/useReconcileProgress'
 
 /*
   IMPORTANT: This page needs to run a GraphQL function to create a new user in
@@ -21,6 +22,7 @@ import ProfileCell from 'src/components/ProfileCell'
 
 const ProfilePage = () => {
   const { logIn, logOut, isAuthenticated, loading } = useAuth()
+  const { reconcilePuzzles, progressLoading } = useReconcileProgress()
   const [errorMessage, setErrorMessage] = useState('')
   const emailRef = useRef(null)
 
@@ -37,6 +39,7 @@ const ProfilePage = () => {
       // @NOTE: redirectURI not working with built-in logIn function
       await logIn({ email })
       // function -> reconcile cookies
+      reconcilePuzzles()
     } catch (e) {
       setErrorMessage('Problem sending email')
     }
@@ -44,13 +47,20 @@ const ProfilePage = () => {
 
   return (
     <>
-      {isAuthenticated && (
+      {isAuthenticated && !progressLoading && (
         <div className="mx-auto w-full max-w-lg pb-12">
           <ProfileCell />
+
+          <button
+            className="mx-auto mt-2 block italic text-gray-200 underline transition-colors hover:text-turquoise"
+            onClick={() => reconcilePuzzles()}
+          >
+            Sync your progress
+          </button>
         </div>
       )}
 
-      {loading ? (
+      {loading || progressLoading ? (
         <LoadingIcon />
       ) : (
         <div className="relative text-center">
