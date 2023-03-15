44cb964e2f280cd774e3decb57690ea893e98523
IvySwing <entense@att.net>
2023-03-15 19:02:57 +0000

    update to link

--- api/src/functions/discord-mint-bot.ts
+++ api/src/functions/discord-mint-bot.ts
@@ -44,7 +44,7 @@ export const handler = async (event) => {
     const chain = chainIdLookup[chainId]
 
     const response = await fetch(
-      `https://api.infinitykeys.io/metadata?contractName=achievement&tokenId={tokenId}`
+      `https://api.infinitykeys.io/metadata?contractName=achievement&tokenId=${tokenId}`
     )
     const nftMetadata = (await response.json()) as MetadataResponse
     const image = nftMetadata.image
