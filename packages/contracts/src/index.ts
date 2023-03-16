d4d32d2501e4879f3b3c1078fd86b956d45de77b
Christopher Bloom <bloomcb@gmail.com>
2023-03-16 13:56:51 -0700

    packages/ in proper state

--- packages/contracts/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * as factories from './generated/factories'
-export { IKAchievementABI__factory } from './generated/factories/IKAchievementABI__factory'
-export { default as balanceOf1155 } from './balanceOf1155.json'
-export { default as balanceOf721 } from './balanceOf721.json'
--- packages/react-lens-share-button/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export type {
-  LensShareProps,
-  BuildLensShareUrlProps,
-  LensIconProps,
-} from './types'
-export { LensShareButton } from './LensShareButton'
-export { LensIcon } from './LensIcon'
-export { buildLensShareUrl } from './utils'
