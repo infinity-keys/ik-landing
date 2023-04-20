72890e200e3dc2f0ffde9836b8067033d367a164
Tawnee <allprt89@gmail.com>
2023-04-20 12:41:21 -0600

    update routes

--- web/src/pages/UnderConstructionPage/UnderConstructionPage.tsx
+++ web/src/pages/UnderConstructionPage/UnderConstructionPage.tsx
@@ -8,7 +8,7 @@ const UnderConstructionPage = () => {
         description="UnderConstruction page"
       />
 
-      <h1 className="text-4xl font-bold">Under Construction</h1>
+      <h1 className="mb-2 text-4xl font-bold">Under Construction</h1>
       <p>We will be back shortly. Thank you for your patience.</p>
     </div>
   )
