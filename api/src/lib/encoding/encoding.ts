8dc36cdd4f828da60566480882fa323e29b32e0e
richardburd <rick.a.burd@gmail.com>
2023-04-28 13:23:06 -0700

    if-not-throw-error on secret key

--- api/src/lib/encoding/encoding.ts
+++ api/src/lib/encoding/encoding.ts
@@ -4,14 +4,16 @@ import { compress, decompress } from 'qfs-compression'
 
 const { INFINITY_KEYS_SECRET } = process.env
 
+// available for entire file
+if (!INFINITY_KEYS_SECRET) {
+  throw new Error('INFINITY_KEYS_SECRET is not set')
+}
+
 // @TODO: can buffer from one function feed to the other
 export const compressAndEncryptText = (plaintext: string) => {
   // Compress
   const compressedText = compress(Buffer.from(plaintext)).toString('base64')
   // Encrypt
-  if (!INFINITY_KEYS_SECRET) {
-    throw new Error('INFINITY_KEYS_SECRET is not set')
-  }
   const cyphertext = aes
     .encrypt(compressedText, INFINITY_KEYS_SECRET)
     .toString()
