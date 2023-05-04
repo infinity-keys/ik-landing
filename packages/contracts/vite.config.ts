aeeb0f40f0e9e2603f060f950c444ac148baf055
richardburd <rick.a.burd@gmail.com>
2023-05-03 17:18:33 -0700

    failed attempts to copy json files

--- packages/contracts/vite.config.ts
+++ packages/contracts/vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 
 import typescript from '@rollup/plugin-typescript'
+import copy from 'rollup-plugin-copy'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 import { defineConfig } from 'vite'
 
@@ -23,13 +24,25 @@ export default defineConfig({
     reportCompressedSize: process.env.NODE_ENV !== 'development',
 
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: [
+        path.resolve(__dirname, 'src/index.ts'),
+        path.resolve(__dirname, 'src/copy-json.ts'),
+      ],
       formats: ['es', 'cjs'],
       fileName: 'index',
     },
+
     rollupOptions: {
       external: [],
       plugins: [
+        copy({
+          targets: [
+            {
+              src: 'src/*.json',
+              dest: './dist',
+            },
+          ],
+        }),
         typescriptPaths({
           preserveExtensions: true,
         }),
