e1b9c35977b2145ae1ddd16c38c60bbefdc89858
Tawnee <allprt89@gmail.com>
2023-05-09 13:45:04 -0600

    remove toast provider, update web login

--- web/src/providers/auth/auth.tsx
+++ web/src/providers/auth/auth.tsx
@@ -3,9 +3,19 @@ import { PropsWithChildren } from 'react'
 import { AuthProvider as RedwoodAuthProvider } from 'src/auth'
 import { getErrorResponse } from 'src/utils/helpers'
 
-export const logIn = async (attributes) => {
+export const logIn = async ({
+  type,
+  code,
+  state,
+}: {
+  type?: string
+  code?: string
+  state?: string
+}) => {
   try {
-    const { type, code, state } = attributes
+    if (!code || !state || !type)
+      throw new Error('logIn() code, type, or state not provided.')
+
     const response = await fetch(`${global.RWJS_API_URL}/auth`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
