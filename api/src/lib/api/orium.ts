4914b3f8d2c3d5d470bc3f4ef3099abe10006ce9
Tawnee <allprt89@gmail.com>
2023-05-18 13:30:15 -0600

    add create and format methods to orium types

--- api/src/lib/api/orium.ts
+++ api/src/lib/api/orium.ts
@@ -10,36 +10,80 @@ const REFERER =
 
 const ORIGIN = 'https://subgraph.satsuma-prod.com'
 
-const HAS_CREATED_SCHOLARSHIP_QUERY = JSON.stringify({
-  query: 'query Accounts ($limit: Int) { accounts(first: $limit) { id } }',
-  operationName: 'Accounts',
-  variables: { limit: 2 },
-})
+const HAS_CREATED_SCHOLARSHIP = {
+  createBody: (owner: string) => {
+    return JSON.stringify({
+      query:
+        'query GetScholarships ($first: Int, $owner: String) { scholarshipPrograms(first: 1, where:{ owner: $owner }) { id } }',
+      variables: { first: 1, owner },
+    })
+  },
+  formatResponse: (data: object) => {
+    if (
+      !('scholarshipPrograms' in data) ||
+      !Array.isArray(data.scholarshipPrograms)
+    ) {
+      throw new Error('Error formatting response for HAS_CREATED_SCHOLARSHIP')
+    }
 
-const HAS_CREATED_VAULT_QUERY = JSON.stringify({
-  query: 'query Accounts ($limit: Int) { accounts(first: $limit) { id } }',
-  operationName: 'Accounts',
-  variables: { limit: 2 },
-})
+    return { success: !!data.scholarshipPrograms.length }
+  },
+}
 
-const HAS_DEPOSITED_NFT_QUERY = JSON.stringify({
-  query: 'query Accounts ($limit: Int) { accounts(first: $limit) { id } }',
-  operationName: 'Accounts',
-  variables: { limit: 2 },
-})
+const HAS_CREATED_VAULT = {
+  createBody: (owner: string) => {
+    return JSON.stringify({
+      query:
+        'query GetVaults ($first: Int, $owner: String) { vaults(first: 1, where:{ owner: $owner }) { id } }',
+      variables: { first: 1, owner },
+    })
+  },
+  formatResponse: (data: object) => {
+    if (!('vaults' in data) || !Array.isArray(data.vaults)) {
+      throw new Error('Error formatting response for HAS_CREATED_VAULT')
+    }
+
+    return { success: !!data.vaults.length }
+  },
+}
+
+const HAS_DEPOSITED_NFT = {
+  createBody: (owner: string) => {
+    return JSON.stringify({
+      query:
+        'query GetDepositedNfts ($first: Int, $owner: String) { nfts(first: $first, where: {vault_: {owner: $owner}}) { id } }',
+      variables: { first: 1, owner },
+    })
+  },
+  formatResponse: (data: object) => {
+    if (!('nfts' in data) || !Array.isArray(data.nfts)) {
+      throw new Error('Error formatting response for HAS_DEPOSITED_NFT')
+    }
+
+    return { success: !!data.nfts.length }
+  },
+}
 
 const checkTypeLookup: {
-  [key in OriumCheckType]: string
+  [key in OriumCheckType]: {
+    createBody: (owner: string) => string
+    formatResponse: (data: object) => { success: boolean }
+  }
 } = {
-  HAS_CREATED_SCHOLARSHIP: HAS_CREATED_SCHOLARSHIP_QUERY,
-  HAS_CREATED_VAULT: HAS_CREATED_VAULT_QUERY,
-  HAS_DEPOSITED_NFT: HAS_DEPOSITED_NFT_QUERY,
+  HAS_CREATED_SCHOLARSHIP,
+  HAS_CREATED_VAULT,
+  HAS_DEPOSITED_NFT,
 }
 
 export const checkOriumApi = async (
   account: string,
   checkType: OriumCheckType
-) => {
+): Promise<{
+  success?: boolean
+  errors?: string[]
+}> => {
+  const oriumCheck = checkTypeLookup[checkType]
+
   try {
     const options = {
       method: 'POST',
@@ -49,18 +93,23 @@ export const checkOriumApi = async (
         origin: ORIGIN,
         referer: REFERER,
       },
-      body: checkTypeLookup[checkType],
+      body: oriumCheck.createBody(account),
     }
 
     const res = await fetch(ORIUM_API_URL, options)
-    const data = await res.json()
-    console.log(JSON.stringify(data, null, 2))
+    const { data } = await res.json()
 
-    return {
-      success: false,
+    if (typeof data !== 'object' || data === null) {
+      throw new Error('Error fetching data from Orium.')
     }
-  } catch {
-    logger.error(`Failed Orium api check for ${account}`)
+
+    return oriumCheck.formatResponse(data)
+  } catch (e) {
+    logger.error(`Failed Orium api check for ${account}`, e)
+
+    if (e instanceof Error) {
+      return { errors: [e.message] }
+    }
     return { errors: ['Error checking Orium.'] }
   }
 }
