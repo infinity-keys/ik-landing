25501913a1663f48efef467210a193ebb005a7fb
Christopher Bloom <bloomcb@gmail.com>
2023-05-22 08:56:38 -0700

    check-nft typescript tune up

--- api/src/lib/web3/check-nft.ts
+++ api/src/lib/web3/check-nft.ts
@@ -1,7 +1,7 @@
 import { balanceOf1155, balanceOf721 } from '@infinity-keys/contracts'
 import { ethers } from 'ethers'
 import fetch from 'node-fetch'
-import { Maybe, NftCheckDatum } from 'types/graphql'
+import { NftCheckDatum } from 'types/graphql'
 import { z } from 'zod'
 
 import { providerLookup } from 'src/lib/lookups'
@@ -17,13 +17,18 @@ const poapResData = z
     'Should either return error or tokenId.'
   )
 
+type CheckReturn = {
+  nftPass: boolean
+  errors: string[]
+}
+
 const checkPoap = async ({
   account,
   poapEventId,
 }: {
   account: string
   poapEventId: string
-}) => {
+}): Promise<CheckReturn> => {
   const url = new URL(
     `/actions/scan/${account}/${poapEventId}`,
     'https://api.poap.tech'
@@ -42,11 +47,14 @@ const checkPoap = async ({
     const data = await res.json()
     poapResData.parse(data)
 
-    if (data.error) return { nftPass: false }
+    if (data.error) return { nftPass: false, errors: [] }
 
-    return { nftPass: true }
+    return { nftPass: true, errors: [] }
   } catch (e) {
-    return { errors: ['There was a problem checking your POAP'] }
+    return {
+      nftPass: false,
+      errors: ['There was a problem checking your POAP'],
+    }
   }
 }
 
@@ -58,9 +66,9 @@ const checkContract = async ({
 }: {
   account: string
   chainId: number
-  tokenId?: Maybe<number>
+  tokenId?: number
   contractAddress: string
-}) => {
+}): Promise<CheckReturn> => {
   // No token Id for ERC721
   // @NOTE: tokenId can be zero, which is falsy. Can't just check for existence tokenId
   const abi = typeof tokenId === 'number' ? balanceOf1155 : balanceOf721
@@ -76,9 +84,9 @@ const checkContract = async ({
 
     const balance = parseInt(await contract.balanceOf(...args), 10)
 
-    return { nftPass: balance > 0 }
+    return { nftPass: balance > 0, errors: [] }
   } catch {
-    return { errors: ['There was a problem checking your NFT'] }
+    return { nftPass: false, errors: ['There was a problem checking your NFT'] }
   }
 }
 
@@ -96,9 +104,14 @@ export const checkNft = async ({
       if (poapEventId) return checkPoap({ account, poapEventId })
 
       if (!chainId || !contractAddress)
-        return { errors: ['Missing nft check data'] }
+        return { nftPass: false, errors: ['Missing nft check data'] }
 
-      return checkContract({ account, chainId, tokenId, contractAddress })
+      return checkContract({
+        account,
+        chainId,
+        tokenId: tokenId ?? undefined,
+        contractAddress,
+      })
     }
   )
 
