4ba75c458b78f0a0b1f18901e2a5809327d2f8ff
Tawnee <allprt89@gmail.com>
2023-05-23 12:52:29 -0600

    reset markdown tsx

--- web/src/components/Markdown/Markdown.tsx
+++ web/src/components/Markdown/Markdown.tsx
@@ -10,64 +10,62 @@ type Props = {
   children: string
 }
 
-const Markdown = ({ children }: Props) => {
-  return (
-    <ReactMarkdown
-      components={{
-        a: ({ href, children }) => {
-          if (!href) return null
+const Markdown = ({ children }: Props) => (
+  <ReactMarkdown
+    components={{
+      a: ({ href, children }) => {
+        if (!href) return null
 
-          const gstopEmbed = href.startsWith(
-            'https://www.gstop-content.com/ipfs/'
-          )
-          const internalLink = href.startsWith(IK_CLAIMS_NAMESPACE)
+        const gstopEmbed = href.startsWith(
+          'https://www.gstop-content.com/ipfs/'
+        )
+        const internalLink = href.startsWith(IK_CLAIMS_NAMESPACE)
 
-          const [words] = children
+        const [words] = children
 
-          const isEmbed = typeof words === 'string' && words?.includes('|')
+        const isEmbed = typeof words === 'string' && words?.includes('|')
 
-          if (isEmbed) {
-            const options = words?.split('|')
-            const title = options.length > 0 ? options[0].trim() : 'embed video'
-            const aspectRatio = options[1].trim()
-            if (!aspectRatio)
-              throw new Error(
-                'Aspect ratio required. Please input on right side of |.'
-              )
-            return (
-              <Iframe
-                src={href}
-                title={title}
-                aspect={aspectRatio}
-                sandbox={gstopEmbed ? 'allow-scripts' : undefined}
-              />
+        if (isEmbed) {
+          const options = words?.split('|')
+          const title = options.length > 0 ? options[0].trim() : 'embed video'
+          const aspectRatio = options[1].trim()
+          if (!aspectRatio)
+            throw new Error(
+              'Aspect ratio required. Please input on right side of |.'
             )
-          }
-
-          if (internalLink) {
-            const path = href.split(IK_CLAIMS_NAMESPACE)[1] || '/'
-            return (
-              <Link to={path} className="underline">
-                {children}
-              </Link>
-            )
-          }
-
           return (
-            <a
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-              href={href}
-            >
-              {children}
-            </a>
+            <Iframe
+              src={href}
+              title={title}
+              aspect={aspectRatio}
+              sandbox={gstopEmbed ? 'allow-scripts' : undefined}
+            />
           )
-        },
-      }}
-    >
-      {cleanGqlMarkdown(children)}
-    </ReactMarkdown>
-  )
-}
+        }
+
+        if (internalLink) {
+          const path = href.split(IK_CLAIMS_NAMESPACE)[1] || '/'
+          return (
+            <Link to={path} className="underline">
+              {children}
+            </Link>
+          )
+        }
+
+        return (
+          <a
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={href}
+          >
+            {children}
+          </a>
+        )
+      },
+    }}
+  >
+    {cleanGqlMarkdown(children)}
+  </ReactMarkdown>
+)
 export default Markdown
