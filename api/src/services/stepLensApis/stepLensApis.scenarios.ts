e848555a6c5424e2bf14f00b938013fe51087d3c
Tawnee <allprt89@gmail.com>
2023-06-06 17:57:16 -0600

    linter fix

--- api/src/services/stepLensApis/stepLensApis.scenarios.ts
+++ api/src/services/stepLensApis/stepLensApis.scenarios.ts
@@ -1,4 +1,5 @@
 import type { Prisma, StepLensApi } from '@prisma/client'
+
 import type { ScenarioData } from '@redwoodjs/testing/api'
 
 export const standard = defineScenario<Prisma.StepLensApiCreateArgs>({
