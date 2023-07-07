52fb32aa7bf0ec1d63c7648391edce4f2dafa20b
IvySwing <entense@att.net>
2023-07-07 19:53:11 +0000

    ts updates on commands

--- packages/discord-dust-game-bot/commands/user.ts
+++ packages/discord-dust-game-bot/commands/user.ts
@@ -1,14 +1,17 @@
-const { SlashCommandBuilder } = require('discord.js')
+import { Interaction, SlashCommandBuilder } from 'discord.js'
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('user')
-    .setDescription('Provides information about the user.'),
-  async execute(interaction) {
-    // interaction.user is the object representing the User who ran the command
-    // interaction.member is the GuildMember object, which represents the user in the specific guild
-    await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
-    )
-  },
+export const data = new SlashCommandBuilder()
+  .setName('user')
+  .setDescription('Provides information about the user.')
+export async function execute(interaction: Interaction) {
+  // if we're not repliable then exit
+  if (!interaction.isRepliable()) {
+    return
+  }
+  // interaction.user is the object representing the User who ran the command
+  // interaction.member is the GuildMember object, which represents the user in the specific guild
+
+  await interaction.reply(
+    `This command was run by ${interaction.user.username}, who joined on ${interaction.user.createdAt}.`
+  )
 }
