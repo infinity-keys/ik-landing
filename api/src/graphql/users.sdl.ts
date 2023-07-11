836bbbd6f27570c99689bca65d310bee3970e1ad
Tawnee <allprt89@gmail.com>
2023-07-11 08:58:00 -0600

    make roles optional

--- api/src/graphql/users.sdl.ts
+++ api/src/graphql/users.sdl.ts
@@ -62,7 +62,7 @@ export const schema = gql`
     email: String
     lensProfile: String
     avatar: String
-    roles: [SiteRole]!
+    roles: [SiteRole]
     refreshToken: String
     accessToken: String
   }
