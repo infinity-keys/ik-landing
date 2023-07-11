55faad89901b9aebc464406f48c9dc924c9300f3
Tawnee <allprt89@gmail.com>
2023-07-11 08:27:21 -0600

    add upsert and use in lens connect

--- api/src/graphql/users.sdl.ts
+++ api/src/graphql/users.sdl.ts
@@ -63,7 +63,7 @@ export const schema = gql`
     email: String
     lensProfile: String
     avatar: String
-    roles: [SiteRole]!
+    roles: [SiteRole]
     refreshToken: String
     accessToken: String
   }
---
+++ api/src/services/ik/lensKeypConnect/lensKeypConnect.ts
@@ -0,0 +1,50 @@
+import { MutationResolvers } from 'types/graphql'
+
+import { AuthenticationError } from '@redwoodjs/graphql-server'
+
+import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
+
+export const upsertLensKeypConnect: MutationResolvers['upsertLensKeypConnect'] =
+  async ({ lensAddress }) => {
+    try {
+      if (!context.currentUser?.id) {
+        throw new AuthenticationError('Must be logged in')
+      }
+
+      const user = await db.user.findUniqueOrThrow({
+        where: { id: context.currentUser.id },
+        select: { address: true },
+      })
+
+      if (!user?.address) {
+        throw new Error("Cannot find user's Keyp wallet")
+      }
+
+      await db.lensKeypConnection.upsert({
+        where: { userId: context.currentUser.id },
+        create: {
+          userId: context.currentUser.id,
+          keypAddress: user.address,
+          lensAddress,
+        },
+        update: {
+          lensAddress,
+        },
+      })
+
+      return {
+        success: true,
+      }
+    } catch (e) {
+      logger.error('Error in `upsertLensKeypConnect`', e)
+
+      return {
+        errors: [
+          ...(e instanceof Error
+            ? [e.message]
+            : ['Error connecting Lens and Keyp accounts']),
+        ],
+      }
+    }
+  }
