8c91d3b9a6c41dddd88410a66761aba60d3c8dd5
Tawnee <allprt89@gmail.com>
2023-08-07 12:43:44 -0600

    add new role

--- api/src/graphql/ik/user-custom.sdl.ts
+++ api/src/graphql/ik/user-custom.sdl.ts
@@ -1,5 +1,10 @@
 export const schema = gql`
+  type AddLensFormRoleResponse {
+    success: Boolean
+  }
   type Mutation {
     upsertUser(authId: String!, email: String!): User! @requireAuth
+    addLensFormRole(externalAddress: String): AddLensFormRoleResponse!
+      @requireAuth
   }
 `
