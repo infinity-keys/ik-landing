d4644156a8138b5236c9a3d5193cd23c9dd787aa
Tawnee <allprt89@gmail.com>
2023-08-11 11:08:34 -0600

    remove leftover type

--- api/src/graphql/ik/steps.sdl.ts
+++ api/src/graphql/ik/steps.sdl.ts
@@ -1,8 +1,4 @@
 export const schema = gql`
-  type StepBySlugResponse {
-    puzzleId: String!
-    step: Step!
-  }
   type Query {
     stepBySlug(slug: String!, stepNum: Int!): Step! @requireAuth
   }
