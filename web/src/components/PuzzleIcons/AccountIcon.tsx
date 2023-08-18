7e711fe1432d7172ce202f9ca4821407042f2a85
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2023-08-18 14:12:47 -0400

    itsmekori/eng-1004-bring-in-puzzle-landing-icons (#999)
    created & added puzzles svg icons

---
+++ web/src/components/PuzzleIcons/AccountIcon.tsx
@@ -0,0 +1,12 @@
+const AccountIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="none">
+    <path
+      stroke="#FAFAF9"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="M9.713 37.898A30.97 30.97 0 0 1 25 33.888a30.958 30.958 0 0 1 15.287 4.01m-8.62-17.342a6.666 6.666 0 1 1-13.333 0 6.666 6.666 0 0 1 13.333 0ZM45 25a20 20 0 1 1-40 0 20 20 0 0 1 40 0Z"
+    />
+  </svg>
+)
+export default AccountIcon
---
+++ web/src/components/PuzzleIcons/TravelIcon.tsx
@@ -0,0 +1,12 @@
+const TravelIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="none">
+    <path
+      stroke="#FAFAF9"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="M18.333 42.222 6.23 36.17A2.222 2.222 0 0 1 5 34.182v-23.92a2.222 2.222 0 0 1 3.216-1.986l10.117 5.057m0 28.89v-28.89m0 28.89 13.334-6.667M18.333 13.333l13.334-6.666m0 28.889 10.117 5.057A2.224 2.224 0 0 0 45 38.627v-23.92a2.222 2.222 0 0 0-1.229-1.987L31.667 6.667m0 28.889V6.666"
+    />
+  </svg>
+)
+export default TravelIcon
