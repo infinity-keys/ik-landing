1772879464a2aed4c8458087fb9042fb2a5d9166
Tawnee <allprt89@gmail.com>
2023-09-12 11:09:18 -0600

    remove old migrations

--- api/db/migrations/20230725185744_remove_anonymous_puzzles/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isAnon` on the `Puzzle` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Puzzle" DROP COLUMN "isAnon";
