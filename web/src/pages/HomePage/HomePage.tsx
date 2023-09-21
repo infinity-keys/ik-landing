568a3999c21c6338859ac7ac92663436e73daa69
Tawnee <allprt89@gmail.com>
2023-09-21 12:32:08 -0600

    style home hero section

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -62,17 +62,20 @@ const HomePage = () => {
     <div>
       <Seo title="Home" />
 
-      <div className="relative z-10 mx-auto max-w-[1440px] overflow-hidden pb-16">
+      <div className="relative mx-auto max-w-[1440px] overflow-hidden pb-16">
         <div className="min-h-screen">
           <Section>
-            <div className="mt-16 max-w-2xl">
-              <h1 className="text-shadow-lg text-8xl font-semibold">
+            <div className="relative z-20 mt-16 max-w-2xl">
+              <h1 className="text-shadow-lg text-3xl font-semibold lg:text-8xl">
                 <Fade inline duration={1.8}>
                   Infinity Keys
                 </Fade>
               </h1>
               <Fade delay={0.8}>
-                <p className="text-shadow-lg mt-4 max-w-lg text-4xl">
+                <p
+                  className="text-shadow-lg mt-4 max-w-lg text-2xl lg:text-4xl"
+                  data-cy="description"
+                >
                   is a no-code creator platform for games, collectors, and
                   digital keys
                 </p>
@@ -81,30 +84,45 @@ const HomePage = () => {
           </Section>
         </div>
 
-        <div className="absolute top-32 right-4 z-[-1]">
+        <div className="absolute top-1/3 -left-8 -right-8 z-10 mx-auto max-w-2xl lg:right-4 lg:top-32 lg:bottom-auto lg:left-auto lg:max-w-none">
           <img src={circle} alt="" className="pointer-events-none block" />
 
-          <img
-            src={medal}
-            alt=""
-            className="absolute top-1/3 left-32 w-48 translate-y-16"
-          />
-          <img
-            src={watch}
-            alt=""
-            className="absolute top-32 right-32 w-48 translate-y-16"
-          />
-          <img
-            src={beaker}
-            alt=""
-            className="absolute bottom-1/4 left-1/2 w-48 translate-y-32 -translate-x-8"
-          />
+          <div className="group absolute top-[36%] left-[13%] w-24 md:w-36 lg:w-48">
+            <Fade>
+              <img
+                src={medal}
+                alt=""
+                className="transition-transform ease-in-out group-hover:scale-90"
+              />
+            </Fade>
+          </div>
+
+          <div className="group absolute top-[20%] right-[14%] w-24 md:w-36 lg:w-48">
+            <Fade delay={0.3}>
+              <img
+                src={watch}
+                alt=""
+                className="transition-transform ease-in-out group-hover:scale-90"
+              />
+            </Fade>
+          </div>
+          <div className="group absolute bottom-[12%] left-[46%] w-24 md:w-36 lg:w-48">
+            <Fade delay={0.6}>
+              <img
+                src={beaker}
+                alt=""
+                className="transition-transform ease-in-out group-hover:scale-90"
+              />
+            </Fade>
+          </div>
         </div>
       </div>
 
       <Section>
         <div className="mx-auto max-w-5xl">
-          <h2 className="pl-2 pb-10 text-3xl font-semibold">How It Works</h2>
+          <h2 className="pl-2 pb-10 text-3xl font-semibold lg:text-5xl">
+            How It Works
+          </h2>
           <div className="flex flex-col items-center gap-2 md:flex-row md:justify-center lg:gap-6">
             {benefits.map((data, index) => (
               <BenefitCard {...data} key={data.title} delay={index * 0.3} />
