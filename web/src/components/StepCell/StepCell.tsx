0ac394e4b2d80f22a05c47e28908e5989659a011
Tawnee <allprt89@gmail.com>
2023-10-10 12:23:21 -0600

    use previous failure logic

--- web/src/components/StepCell/StepCell.tsx
+++ web/src/components/StepCell/StepCell.tsx
@@ -3,8 +3,10 @@ import type {
   FindStepBySlugQueryVariables,
 } from 'types/graphql'
 
+import { routes } from '@redwoodjs/router'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
+import Button from 'src/components/Button/Button'
 import LoadingIcon from 'src/components/LoadingIcon/LoadingIcon'
 import OldFormatMessage from 'src/components/OldFormatMessage/OldFormatMessage'
 import StepsLayout from 'src/components/StepsLayout/StepsLayout'
@@ -51,9 +53,22 @@ export const Loading = () => <LoadingIcon />
 
 export const Empty = () => <div>Empty</div>
 
-export const Failure = ({ error }: CellFailureProps) => (
-  <div className="rw-cell-error">{error?.message}</div>
-)
+export const Failure = ({
+  error,
+  queryResult,
+}: CellFailureProps<FindStepBySlugQueryVariables>) => {
+  const slug = queryResult?.variables?.slug
+
+  return (
+    <div className="p-4 text-center">
+      <p className="pb-4">{error?.message}</p>
+      <Button
+        text={slug ? 'Return to Puzzle' : 'Return to Play Page'}
+        to={slug ? routes.puzzleLanding({ slug }) : routes.play()}
+      />
+    </div>
+  )
+}
 
 export const Success = ({
   step,
