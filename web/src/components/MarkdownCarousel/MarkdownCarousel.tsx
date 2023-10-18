68b9026daa132f523349aec1633084da8f7dd495
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2023-10-18 12:57:38 -0400

    itsmekori/eng-1062-remove-slider-arrows-on-firstlast-pages (#1047)
    * update logic and styling for left and right arrow buttons
    
    * update code for left arrow button
    
    ---------
    
    Co-authored-by: Tawnee <tawnee.a@proton.me>

--- web/src/components/MarkdownCarousel/MarkdownCarousel.tsx
+++ web/src/components/MarkdownCarousel/MarkdownCarousel.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from 'react'
 
 import ArrowLeftIcon from '@heroicons/react/24/outline/ArrowLeftCircleIcon'
 import ArrowRightIcon from '@heroicons/react/24/outline/ArrowRightCircleIcon'
+import clsx from 'clsx'
 import Carousel, { ControlProps } from 'nuka-carousel'
 
 const MarkdownCarousel = ({
@@ -19,35 +20,45 @@ const MarkdownCarousel = ({
       dragging={false}
       renderCenterRightControls={null}
       renderCenterLeftControls={null}
-      renderBottomRightControls={(props: ControlProps) => (
-        <button
-          onClick={() => {
-            if (showOverlay) {
-              setShowOverlay(false)
-            }
-            setSlideIndex(
-              props.currentSlide + 1 === props.slideCount
-                ? props.currentSlide
-                : props.currentSlide + 1
-            )
-            props.nextSlide()
-          }}
-          className="mr-6 p-2 text-stone-50 transition-colors hover:text-stone-400"
-        >
-          <ArrowRightIcon className="h-8 w-8 fill-transparent" />
-        </button>
-      )}
-      renderBottomLeftControls={(props: ControlProps) => {
+      renderBottomRightControls={(props: ControlProps) => {
+        const isLastSlide = props.slideCount === props.currentSlide + 1
         return (
           <button
             onClick={() => {
               if (showOverlay) {
                 setShowOverlay(false)
               }
-              setSlideIndex(props.currentSlide > 0 ? props.currentSlide - 1 : 0)
+              setSlideIndex(
+                isLastSlide ? props.currentSlide : props.currentSlide + 1
+              )
+              props.nextSlide()
+            }}
+            disabled={isLastSlide}
+            className={clsx(
+              'mr-6 p-2 text-stone-50 transition-colors hover:text-stone-400',
+              { 'opacity-0': isLastSlide }
+            )}
+          >
+            <ArrowRightIcon className="h-8 w-8 fill-transparent" />
+          </button>
+        )
+      }}
+      renderBottomLeftControls={(props: ControlProps) => {
+        const isFirstSlide = props.currentSlide === 0
+        return (
+          <button
+            onClick={() => {
+              if (showOverlay) {
+                setShowOverlay(false)
+              }
+              setSlideIndex(!isFirstSlide ? props.currentSlide - 1 : 0)
               props.previousSlide()
             }}
-            className="ml-6 p-2 text-stone-50 transition-colors hover:text-stone-400"
+            disabled={isFirstSlide}
+            className={clsx(
+              'ml-6 p-2 text-stone-50 transition-colors hover:text-stone-400',
+              { 'opacity-0': isFirstSlide }
+            )}
           >
             <ArrowLeftIcon className="h-8 w-8 fill-transparent" />
           </button>
