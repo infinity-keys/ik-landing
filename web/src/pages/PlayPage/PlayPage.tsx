0b3168402211ffda07c262cd702c16da4f9a6df6
IvySwing <entense@att.net>
2023-10-18 21:03:45 +0000

    remove transition code block

--- web/src/pages/PlayPage/PlayPage.tsx
+++ web/src/pages/PlayPage/PlayPage.tsx
@@ -1,6 +1,3 @@
-import { Fragment, useState } from 'react'
-
-import { Transition } from '@headlessui/react'
 import { PAGINATION_COUNTS } from '@infinity-keys/constants'
 
 import RewardablesCell from 'src/components/RewardablesCell'
@@ -11,7 +8,6 @@ export enum AlphaModalStatus {
 }
 
 const PlayPage = ({ count, page }: { count?: number; page?: number }) => {
-  const [isHidden, setIsHidden] = useState(true)
   const [smallestThumbnailCount] = PAGINATION_COUNTS
   const perPage = count ?? smallestThumbnailCount
   const pageNum = page ?? 1
@@ -26,35 +22,6 @@ const PlayPage = ({ count, page }: { count?: number; page?: number }) => {
         sortType="FEATURED"
         landingRoute={'PLAY'}
       />
-
-      <Transition appear show={!isHidden} as={Fragment}>
-        {/* Backdrop */}
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black bg-opacity-75" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 max-h-screen overflow-y-scroll">
-          <div className="flex min-h-full items-center justify-center p-8 pt-12 text-center text-gray-100">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            ></Transition.Child>
-          </div>
-        </div>
-      </Transition>
     </>
   )
 }
