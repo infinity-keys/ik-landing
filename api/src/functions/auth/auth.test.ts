3fc0fee4b91b791444027f1e18b07a68512aa4f9
Tawnee <allprt89@gmail.com>
2023-11-13 21:26:02 -0700

    get 200 test to pass

--- api/src/functions/auth/auth.test.ts
+++ api/src/functions/auth/auth.test.ts
@@ -1,14 +1,34 @@
+import * as webhooks from '@redwoodjs/api/webhooks'
 import { mockHttpEvent } from '@redwoodjs/testing/api'
 
 import { handler } from './auth'
 
+jest.spyOn(webhooks, 'verifyEvent').mockReturnValue(true)
+
 describe('webhook function', () => {
-  // it('responds with 200 on valid POST request', async () => {
-  //   const httpEvent = mockHttpEvent(testClerkRequest)
-  //   const response = await handler(httpEvent)
-  //   expect(response.statusCode).toBe(200)
-  //   expect(JSON.parse(response.body || '').data).toBeDefined()
-  // })
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('responds with 200 on valid POST request', async () => {
+    const httpEvent = mockHttpEvent({
+      httpMethod: 'POST',
+      headers: {
+        'svix-id': 'mock_svix_id',
+        'svix-timestamp': 'mock_svix_timestamp',
+        'svix-signature': 'v1,mock_svix_signature',
+      },
+      body: JSON.stringify({
+        username: 'test',
+      }),
+    })
+
+    const response = await handler(httpEvent)
+
+    expect(webhooks.verifyEvent).toHaveBeenCalled()
+    expect(response.statusCode).toBe(200)
+    expect(JSON.parse(response.body || '').data.username).toBe('test')
+  })
 
   it('responds with 403 on non-POST request', async () => {
     const httpEvent = mockHttpEvent({
