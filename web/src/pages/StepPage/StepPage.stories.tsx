b82b3519592c32c773087b52a647d95f6b8507f2
Tawnee <tawnee.a@proton.me>
2023-12-12 11:12:35 -0700

    fix more TS errors (#1101)
    * remove unused email components
    
    * fix ts errors
    
    * fix TS errors in tests (#1104)
    
    * fix storybook tests
    
    * fix type, fix jest tests
    
    ---------
    
    Co-authored-by: Tawnee <allprt89@gmail.com>
    
    ---------
    
    Co-authored-by: Tawnee <allprt89@gmail.com>

--- web/src/pages/StepPage/StepPage.stories.tsx
+++ web/src/pages/StepPage/StepPage.stories.tsx
@@ -1,12 +1,18 @@
-import type { ComponentMeta } from '@storybook/react'
+import type { StoryObj, Meta } from '@storybook/react'
 
 import StepPage from './StepPage'
 
-export const generated = () => {
-  return <StepPage />
+export const generated: StoryObj<typeof StepPage> = {
+  render: (args) => {
+    return <StepPage {...args} />
+  },
 }
 
 export default {
   title: 'Pages/StepPage',
   component: StepPage,
-} as ComponentMeta<typeof StepPage>
+  args: {
+    slug: '/step',
+    step: 1,
+  },
+} as Meta<typeof StepPage>
