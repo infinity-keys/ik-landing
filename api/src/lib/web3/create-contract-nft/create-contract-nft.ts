4b212b468ea17f030b926f8b4d4e207ea53e0998
Tawnee <allprt89@gmail.com>
2024-01-23 09:30:30 -0700

    implement uri loop to ensure token id

--- api/src/lib/web3/create-contract-nft/create-contract-nft.ts
+++ api/src/lib/web3/create-contract-nft/create-contract-nft.ts
@@ -4,6 +4,7 @@ import {
 } from '@infinity-keys/constants'
 import { IKAchievementABI__factory } from '@infinity-keys/contracts'
 import { ethers } from 'ethers'
+import { nanoid } from 'nanoid'
 
 import { logger } from 'src/lib/logger'
 import { providerLookup } from 'src/lib/lookups'
@@ -13,10 +14,12 @@ const { AUTHORIZED_PRIVATE_KEY, NODE_ENV } = process.env
 if (!AUTHORIZED_PRIVATE_KEY && NODE_ENV === 'production') {
   throw new Error('Missing authorized wallet secrets')
 }
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
 
 const wallet = new ethers.Wallet(
   AUTHORIZED_PRIVATE_KEY || '',
-  providerLookup[OPTIMISM_CHAIN_ID]
+  // providerLookup[OPTIMISM_CHAIN_ID]
+  provider
 )
 
 export const createContractNft = async () => {
@@ -26,23 +29,63 @@ export const createContractNft = async () => {
     }
 
     const contract = new ethers.Contract(
-      CONTRACT_ADDRESS_OPTIMISM,
+      // CONTRACT_ADDRESS_OPTIMISM,
+      '0x5FbDB2315678afecb367f032d93F642f64180aa3',
       IKAchievementABI__factory.abi,
       wallet
     )
+    const actualId = (await contract.totalSupplyAll()).length
+    // const probableTokenId = (await contract.totalSupplyAll()).length
+    const probableTokenId = actualId - 4
+    const stringId = nanoid()
 
-    const newTokenId = (await contract.totalSupplyAll()).length
-
+    // @TODO: need to handle this new case in our api function
     const tokenUri = new URL('metadata', 'https://api.infinitykeys.io')
-    tokenUri.searchParams.append('contractName', 'achievement')
-    tokenUri.searchParams.append('tokenId', newTokenId.toString())
+    tokenUri.searchParams.append('id', stringId)
 
-    await contract.addTokenUngated(true, tokenUri)
+    const newNft = await contract.addTokenUngated(true, tokenUri.toString())
+    logger.info(
+      `invoked 'addTokenUngated' for ${stringId}. Transaction hash: ${newNft.hash}`
+    )
 
-    return {
-      tokenId: newTokenId,
+    // @TODO: Can this hang forever? If yes, add a timeout.
+    // @TODO: How long does this take in production?
+    const receipt = await newNft.wait()
+
+    if (receipt.status === 1) {
+      // tx was successful
+      const lastTokenId = (await contract.totalSupplyAll()).length - 1
+      if (probableTokenId === lastTokenId) {
+        // everything worked correctly
+        return {
+          tokenId: probableTokenId,
+        }
+      } else {
+        // other transactions have gone through, check contract to get correct
+        // token id
+        let idToCheck = probableTokenId
+
+        while (idToCheck <= lastTokenId) {
+          const uri = await contract.uri(idToCheck)
+          const url = new URL(uri)
+          const id = url.searchParams.get('id')
+
+          if (id === stringId) {
+            return {
+              tokenId: idToCheck,
+            }
+          }
+
+          idToCheck++
+        }
+
+        throw new Error('There was a problem finding token URI')
+      }
     }
+
+    throw new Error('Transaction failed')
   } catch (error) {
-    logger.error('Error in `createContractNft`', error)
+    console.log(error)
+    // logger.error('Error in `createContractNft`', error)
   }
 }
