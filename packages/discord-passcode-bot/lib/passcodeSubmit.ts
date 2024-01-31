29704726e6b27dc53c5e9dbe7ffa0ff6cd707d05
IvySwing <entense@att.net>
2024-01-31 21:29:18 +0000

    tag user in solution, made incorrect response ephermal, passcode case insensitive and change color of solve button

--- packages/discord-passcode-bot/lib/passcodeSubmit.ts
+++ packages/discord-passcode-bot/lib/passcodeSubmit.ts
@@ -3,9 +3,13 @@ import { ObjectId } from 'mongodb'
 import { Puzzle } from '../models/puzzle'
 
 export async function passcodeSubmit(interaction) {
-  const passcode = interaction.fields.getTextInputValue('passcodeInput')
+  const passcode = interaction.fields
+    .getTextInputValue('passcodeInput')
+    .toLowerCase()
   // Extract puzzle ID from the custom ID
   const puzzleId = interaction.customId.split('passcodeModal-')[1]
+
+  console.log('passcode', passcode)
 
   // Here you would verify the passcode, checking against a value in your database
   const puzzle = await Puzzle.findOne({ _id: new ObjectId(puzzleId) })
@@ -15,11 +19,21 @@ export async function passcodeSubmit(interaction) {
     return
   }
 
-  await interaction.reply(
-    `${
-      puzzle.passcode === passcode
-        ? 'Puzzle solved. Well done.'
-        : "That's incorrect. Try, try again."
-    }`
-  )
+  puzzle.passcode.toLowerCase() === passcode
+    ? await interaction.reply({
+        content: `Puzzle solved. Well done ${interaction.user.username}.`,
+        ephemeral: false,
+      })
+    : await interaction.reply({
+        content: "That's incorrect. Try, try again.",
+        ephemeral: true,
+      })
+
+  // await interaction.reply(
+  //   `${
+  //     puzzle.passcode === passcode
+  //       ? `Puzzle solved. Well done ${interaction.user.username}.`
+  //       : "That's incorrect. Try, try again."
+  //   }`
+  // )
 }
